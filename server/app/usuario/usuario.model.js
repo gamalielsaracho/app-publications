0d2cd3e4f66fefd74e040a2fafed12ed10fa5c8e
gamaliel <fabiogamaliel@gmail.com>
2017-07-30 20:23:18 -0400

    table name usuario changed

--- server/app/usuario/usuario.model.js
+++ server/app/usuario/usuario.model.js
@@ -4,31 +4,31 @@ import connection from '../../config/connection'
 
 exports.crear = (data, callback) => {
 
-	return connection.query('INSERT INTO usuario SET ?', data, callback)
+	return connection.query('INSERT INTO users SET ?', data, callback)
 
 	connection.end()
 }
 
 exports.verificarCorreo = (correo, callback) => {
 
-	return connection.query('select * from usuario where correo = ?', [correo], callback)
+	return connection.query('select * from users where correo = ?', [correo], callback)
 
 	connection.end()
 }
 
 // exports.verificarContrasena = (contrasenaFormulario, callback) => {
-// 	return connection.query('select * from usuario where contrasena = ?', [contrasena], callback)
+// 	return connection.query('select * from users where contrasena = ?', [contrasena], callback)
 // }
 
 exports.listar = (callback) => {
 
-	return connection.query('SELECT * FROM usuario', callback)
+	return connection.query('SELECT * FROM users', callback)
 
 	connection.end()
 }
 
-exports.mostrar = (usuarioId, callback) => {
-	return connection.query('select * from usuario where id = ?', [usuarioId], callback)
+exports.mostrar = (userId, callback) => {
+	return connection.query('select * from users where id = ?', [userId], callback)
 
 	connection.end()
 }
