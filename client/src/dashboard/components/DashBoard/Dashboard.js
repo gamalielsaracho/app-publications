caf0c54c22a88fc552f380ce4057f3cb19b53c27
gamaliel <fabiogamaliel@gmail.com>
2017-08-01 10:07:39 -0400

    menu dashBoard responsive

--- client/src/dashboard/components/DashBoard/Dashboard.js
+++ client/src/dashboard/components/DashBoard/Dashboard.js
@@ -8,18 +8,17 @@ class DashBoard extends Component {
 			textAling: "center"
 		}
 
-		return <div className='container-fruit'>
-			<div className='row'>
-				<div className='#00b0ff light-blue accent-3 col-sm-8 col-md-6 col-lg-2'>
-					<h5 className='center'>Menu</h5>
-					<br/>
+		return <div className='container'>
+			<div className='row justify-lg justify-md justify-sm justify-xs'>
+				<div className='#00b0ff light-blue accent-3 col-xs-12 col-sm-12 col-md-2 col-lg-2'>
 					
 					<ul>
 						<li className='center'><Link style={style} to='/roles'>Roles</Link></li>
 						<li className='center'><Link style={style} to='/usuarios'>Usuarios</Link></li>
 					</ul>
 				</div>
-				<div className='col-sm-8 col-md-6 col-lg-10'>
+
+				<div className=' col-xs-12 col-sm-12 col-md-10 col-lg-10'>
 					<br/>
 					{ this.props.children }
 				</div>
