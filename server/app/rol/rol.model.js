38a55e5d06f0a677964b7c65088020a9e3e75498
gamaliel <fabiogamaliel@gmail.com>
2017-08-05 22:06:08 -0400

    functions deleted

--- server/app/rol/rol.model.js
+++ server/app/rol/rol.model.js
@@ -1,32 +1,18 @@
 import connection from '../../config/connection'
 
-exports.crear = (data, callback) => {
+exports.create = (data, callback) => {
 	return connection.query('INSERT INTO roles SET ?', data, callback)
 
 	connection.end()
 }
 
-exports.listar = (callback) => {
+exports.find = (callback) => {
 	return connection.query('SELECT * FROM roles', callback)
 
 	connection.end()
 }
 
-exports.mostrar = (id, callback) => {
-	console.log("el ide que encontre es: "+id)
-
-	return connection.query('select * from roles where id_rol = ?', [id], callback)
-
-	connection.end()
-}
-
-exports.eliminar = (id, callback) => {
-	return connection.query('delete from roles where id_rol = ?', [id], callback)
-
-	connection.end()
-}
-
-exports.editar = (data, callback) => {
+exports.update = (data, callback) => {
 	return connection.query('update roles set nombre = ? where id_rol = ?', [data.nombre, data.id_rol], callback)
 
 	connection.end()
