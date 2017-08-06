191d5992f44f0574703832001ced03ddb53e7851
gamaliel <fabiogamaliel@gmail.com>
2017-08-05 22:06:52 -0400

    query global function called

--- server/app/rol/rol.sockets.js
+++ server/app/rol/rol.sockets.js
@@ -1,77 +1,80 @@
 import Rol from './rol.model'
 
+import Query from '../queries'
+
+const table = 'roles'
+const fieldId = 'id_rol'
+
 export default (socket, io) => {
 	
-	console.log('LA QUE AHORA ESTÁ ES: '+socket.usuario)
+		function roles() {
+			Query.find(table, (err, roles) => {
+				if(err) {
+					console.log(err)
+				
+					socket.emit('listar_roles', { error: 'Lo sentimos, acurrió un error. intente nuevamente.' })
+					return
+				}
 
-	function roles() {
-		Rol.listar((err, roles) => {
-			if(err) {
-				console.log(err)
-			
-				socket.emit('listar_roles', { error: 'Lo sentimos, acurrió un error. intente nuevamente.' })
-				return
-			}
+				// console.log(roles)
 
-			// console.log(roles)
-
-			io.sockets.emit('listar_roles', { roles: roles })
-		})
-	}
+				io.sockets.emit('listar_roles', { roles: roles })
+			})
+		}
 	
-	roles()
+		roles()
 
 
-	socket.on('crear_rol', function(data) {
-		Rol.crear(data, (err, rol) => {
-			if(err) {
-				socket.emit('crear_rol', { error: 'Ocurrió un error, intente nuevamente' })
-				return
-			}
+		socket.on('crear_rol', function(data) {
+			Rol.create(data, (err, rol) => {
+				if(err) {
+					socket.emit('crear_rol', { error: 'Ocurrió un error, intente nuevamente' })
+					return
+				}
 
-			socket.emit('crear_rol', { mensaje: 'Se agregó exitósamente.' })
-		
-			roles()
+				socket.emit('crear_rol', { mensaje: 'Se agregó exitósamente.' })
+			
+				roles()
+			})
 		})
-	})
 
 
-	socket.on('mostrar_rol', (data) => {
-		Rol.mostrar(data.id_rol, (err, rol) => {
-			if(err) {
-				socket.emit('mostrar_rol', { error: 'Ocurrió un error, intente nuevamente' })
-				return
-			}
+		socket.on('mostrar_rol', (data) => {
+			Query.findById(table, fieldId, data.id_rol, (err, rol) => {
+				if(err) {
+					socket.emit('mostrar_rol', { error: 'Ocurrió un error, intente nuevamente' })
+					return
+				}
 
-			socket.emit('mostrar_rol', rol[0])
+				socket.emit('mostrar_rol', rol[0])
+			})
 		})
-	})
 
 
-	socket.on('eliminar_rol', (data) => {
-		Rol.eliminar(data.id_rol, (err) => {
-			if(err) {
-				socket.emit('eliminar_rol', { error: 'Ocurrió un error, intente nuevamente' })
-				return
-			}
+		socket.on('eliminar_rol', (data) => {
+			Query.delete(table, fieldId, data.id_rol, (err) => {
+				if(err) {
+					socket.emit('eliminar_rol', { error: 'Ocurrió un error, intente nuevamente' })
+					return
+				}
 
-			socket.emit('eliminar_rol', { mensaje: 'Se Eliminó exitósamente.' })
+				socket.emit('eliminar_rol', { mensaje: 'Se Eliminó exitósamente.' })
 
-			roles()
+				roles()
+			})
 		})
-	})
 
 
-	socket.on('editar_rol', (data) => {
-		Rol.editar(data, (err) => {
-			if(err) {
-				socket.emit('editar_rol', { error: 'Ocurrió un error, intente nuevamente' })
-				return
-			}
+		socket.on('editar_rol', (data) => {
+			Rol.update(data, (err) => {
+				if(err) {
+					socket.emit('editar_rol', { error: 'Ocurrió un error, intente nuevamente' })
+					return
+				}
 
-			socket.emit('editar_rol', { mensaje: 'Se actualizó exitósamente.' })
-		
-			roles()
+				socket.emit('editar_rol', { mensaje: 'Se actualizó exitósamente.' })
+			
+				roles()
+			})
 		})
-	})
 }
