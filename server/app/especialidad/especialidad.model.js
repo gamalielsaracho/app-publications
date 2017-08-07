927a0ce91852ac85dbfee2e82153834348683846
gamaliel <fabiogamaliel@gmail.com>
2017-08-07 16:21:20 -0400

    sql query added

--- server/app/especialidad/especialidad.model.js
+++ server/app/especialidad/especialidad.model.js
@@ -1,5 +1,17 @@
 import connection from '../../config/connection'
 
+exports.find = (callback) => {
+	return connection.query('SELECT * FROM especialidades', callback)
+
+	connection.end()
+}
+
+exports.findById = (idEspecialidad, callback) => {
+	return connection.query('select * from especialidades where id_especialidad = ?', [idEspecialidad], callback)
+
+	connection.end()
+}
+
 exports.create = (data, callback) => {
 	return connection.query('INSERT INTO especialidades SET ?', data, callback)
 
@@ -7,7 +19,19 @@ exports.create = (data, callback) => {
 }
 
 exports.update = (data, callback) => {
-	return connection.query('update especialidades set descripcion = ? where id_especialidad = ?', [data.nombre, data.id_especialidad], callback)
+
+	return connection.query('update especialidades set descripcion = ? where id_especialidad = ?', [data.descripcion, data.id_especialidad], callback)
 
 	connection.end()
 }
+
+exports.delete = (idEspecialidad, callback) => {
+	return connection.query('delete from especialidades where id_especialidad = ?', [idEspecialidad], callback)
+
+	connection.end()
+}
+
+
+
+
+
