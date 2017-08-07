f7ccd06ba71781284a79449b5c5098ca00e5492a
gamaliel <fabiogamaliel@gmail.com>
2017-08-07 16:26:06 -0400

    especialidad and ciudad added, unsuario name changed to personal

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,14 +2,18 @@ import { combineReducers } from 'redux'
 
 import { reducer as formReducer } from 'redux-form'
 
-import usuarioReducer from '../usuario/reducer'
+import personalReducer from '../usuario/reducer'
 import rolReducer from '../rol/reducer'
+import especialidadReducer from '../especialidades/reducer'
+import ciudadReducer from '../ciudad/reducer'
 
 
 const rootReducer = combineReducers({
 	form: formReducer,
-	usuario: usuarioReducer,
-	rol: rolReducer
+	personal: personalReducer,
+	rol: rolReducer,
+	especialidad: especialidadReducer,
+	ciudad: ciudadReducer
 })
 
 export default rootReducer
