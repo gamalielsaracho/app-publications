c4fac6885371a8bc01a49e9c6a03933b4499d471
gamaliel <fabiogamaliel@gmail.com>
2017-08-07 16:20:26 -0400

    sql queries created

--- server/app/rol/rol.model.js
+++ server/app/rol/rol.model.js
@@ -1,19 +1,33 @@
 import connection from '../../config/connection'
 
-exports.create = (data, callback) => {
-	return connection.query('INSERT INTO roles SET ?', data, callback)
-
-	connection.end()
-}
-
 exports.find = (callback) => {
 	return connection.query('SELECT * FROM roles', callback)
 
 	connection.end()
 }
 
+exports.findById = (idRol, callback) => {
+	return connection.query('select * from roles where id_rol = ?', [idRol], callback)
+
+	connection.end()
+}
+
+exports.create = (data, callback) => {
+	return connection.query('INSERT INTO roles SET ?', data, callback)
+
+	connection.end()
+}
+
 exports.update = (data, callback) => {
-	return connection.query('update roles set descripcion = ? where id_rol = ?', [data.nombre, data.id_rol], callback)
+	return connection.query('update roles set descripcion = ? where id_rol = ?', [data.descripcion, data.id_rol], callback)
+
+	connection.end()
+}
+
+exports.delete = (idRol, callback) => {
+	console.log("el id es :"+idRol)
+	
+	return connection.query('DELETE FROM roles WHERE id_rol = ?', [idRol], callback)
 
 	connection.end()
 }
