074d84bf591c632580c925774349778d4032e4f8
gamaliel <fabiogamaliel@gmail.com>
2017-08-08 04:54:26 -0400

    new container-fluid and row

--- client/src/dashboard/components/DashBoard/Dashboard.js
+++ client/src/dashboard/components/DashBoard/Dashboard.js
@@ -5,8 +5,8 @@ import Menu from '../Menu'
 class DashBoard extends Component {
 	render() {
 
-		return <div className='container-fluid container-max'>
-			<div className='row justify-lg justify-md justify-sm justify-xs'>
+		return <div className='container-fluid'>
+			<div className='row'>
 				<Menu/>
 
 				<div className='col-xs-12 col-sm-12 col-md-10 col-lg-10'>
