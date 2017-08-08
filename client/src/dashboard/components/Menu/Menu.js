a5dc2a7c6028ff73266a19428ed4cc247712ebee
gamaliel <fabiogamaliel@gmail.com>
2017-08-08 04:53:59 -0400

    new structure

--- client/src/dashboard/components/Menu/Menu.js
+++ client/src/dashboard/components/Menu/Menu.js
@@ -3,82 +3,23 @@ import { Link } from 'react-router'
 
 class Menu extends Component {
 	render() {
-		let styles = {
-			menuContainer: {
-				"padding": "1em"
-				// "height": '570px'
-			},
-			btnContainer: {
-				padding: '0em .5em .5em 1.5em',
-				color: "#fff"
-			},
-			btnText: {
-				color: "#fff"
-			}
-		}
 
-		let po = {
-			border: '1px solid black'
-		}
-
-		return <div style={styles.menuContainer} className='#00b0ff light-blue accent-3 col-xs-12 col-sm-12 col-md-2 col-lg-2'>
-			<div className='row justify-lg justify-md between-sm justify-xs'>
-				<div className='col-xs-12 col-sm-4 col-md-12 col-lg-12'>
-					<h5 className='center'>Hola LOP</h5>
+		return <div className='col-sm-3 col-md-2 back-green'>
+			<div className='row'>
+				<div className='col-xs-4 col-sm-6 col-md-6 col-lg-6'>
+					<br/>
+					<img className='img-circle img-responsive' src='http://localhost:8080/images/gama.jpg'/>
 				</div>
-					
-				<div className='col-xs-12 col-sm-4 col-md-12 col-lg-12'>
-					<ul className='row justify-lg justify-md justify-sm justify-xs'>
-								
-						<div className='col-xs-6 col-sm-6 col-md-12 col-lg-12'>
-							<div className='row around-lg around-md around-sm around-xs'>
-								<div className='col-xs-6 col-sm-7 col-md-9 col-lg-9'>
-									<Link style={styles.btnText} to='/usuarios'>Usuarios</Link>
-								</div>
-								<div className='col-xs-6 col-sm-2 col-md-2 col-lg-2'>
-									<i className="sm material-icons">insert_chart</i>
-								</div>
-							</div>
-						</div>
-
-						<div className='col-xs-6 col-sm-6 col-md-12 col-lg-12'>
-							<div className='row around-lg around-md around-sm around-xs'>
-								<div className='col-xs-6 col-sm-7 col-md-9 col-lg-9'>
-									<Link style={styles.btnText} to='/roles'>Roles</Link>
-								</div>
-								<div className='col-xs-6 col-sm-2 col-md-2 col-lg-2'>
-									<i className="sm material-icons">insert_chart</i>
-								</div>
-							</div>
-						</div>
-
-						<div className='col-xs-6 col-sm-6 col-md-12 col-lg-12'>
-							<div className='row around-lg around-md around-sm around-xs'>
-								<div className='col-xs-6 col-sm-7 col-md-9 col-lg-9'>
-									<Link style={styles.btnText} to='/especialidades'>Especialidades</Link>
-								</div>
-								<div className='col-xs-6 col-sm-2 col-md-2 col-lg-2'>
-									<i className="sm material-icons">insert_chart</i>
-								</div>
-							</div>
-						</div>
-
-						<div className='col-xs-6 col-sm-6 col-md-12 col-lg-12'>
-							<div className='row around-lg around-md around-sm around-xs'>
-								<div className='col-xs-6 col-sm-7 col-md-9 col-lg-9'>
-									<Link style={styles.btnText} to='/ciudades'>Ciudades</Link>
-								</div>
-								<div className='col-xs-6 col-sm-2 col-md-2 col-lg-2'>
-									<i className="sm material-icons">insert_chart</i>
-								</div>
-							</div>
-						</div>
-
-
-					</ul>
-				</div>
-						
 			</div>
+			<h4 className='text-center'>Hola LOP</h4>
+					
+			<ul className="nav nav-sidebar">
+				<li><Link to='/usuarios'>Usuarios <span className="badge">14</span></Link></li>
+				<li><Link to='/roles'>Roles <span className="badge">1</span></Link></li>
+				<li><Link to='/especialidades'>Especialidades <span className="badge">0</span></Link></li>
+				<li><Link to='/ciudades'>Ciudades <span className="badge">8</span></Link></li>
+	        </ul>
+						
 		</div>
 	}
 }
