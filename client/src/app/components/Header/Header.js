6fbe7293905efe9d143b24a4e3ba4d97a4092456
gamaliel <fabiogamaliel@gmail.com>
2017-08-10 04:07:51 -0400

    styles deleted and new structure

--- client/src/app/components/Header/Header.js
+++ client/src/app/components/Header/Header.js
@@ -15,42 +15,8 @@ class Header extends Component {
 	}
 
 	render() {
-		let styles = {
-			headerContainer1: {
-				// backgroundColor:'#F43838',
-				background:'url(https://dempsey-siders.com/wp-content/uploads/2014/08/dempseyPageHeaderHealth.jpg)',
-				backgroundSize:'cover',
-				backgroundPosition:'center',
-				backgroundRepeat:'no-repeat',
-				height:'100%',
-				paddingTop:'80px',
-				width:'auto'
-			},
-			headerContainer2: {
-				// backgroundColor:'#F43838',
-				background:'url(http://localhost:8080/images/header.jpg)',
-				backgroundSize:'cover',
-				backgroundPosition:'center',
-				backgroundRepeat:'no-repeat',
-				height:'100%',
-				paddingTop:'80px',
-				width:'auto'
-			}
-		}
-
-		return <div id='inicio' className="carousel carousel-slider center" data-indicators="true">
-		    <div className="carousel-item" href="#one!">
-		    	<div style={styles.headerContainer1}>
-			      <h2>First Panel</h2>
-			      <p className="white-text">This is your first panel</p>
-		    	</div>
-		    </div>
-		    <div className="carousel-item red white-text" href="#two!">
-		    	<div style={styles.headerContainer2}>
-		      		<h2>Second Panel</h2>
-			    	<p className="white-text">This is your second panel</p>
-		    	</div>
-		    </div>
+		return <div id='inicio' className="container-fluid wide">
+			<h1 className='text-center'>Hola</h1>
   		</div>
 	}
 }
