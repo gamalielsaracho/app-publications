bad62133e498443623d71eea57ea16ca7fa0b082
gamaliel <fabiogamaliel@gmail.com>
2017-08-10 04:11:42 -0400

    validations user register form

--- client/src/usuario/components/Registrar/container.js
+++ client/src/usuario/components/Registrar/container.js
@@ -2,32 +2,97 @@ import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 
 import {
+  listarEspecialidades
+} from '../../../especialidades/actions'
+
+import {
+  listarRoles
+} from '../../../rol/actions'
+
+import {
 	registrarPersonal
 } from '../../actions'
 
+
 import Registrar from './Registrar'
+
+// nroDocumento
+// id_tipoDocumento
+// id_especialidad
+// id_rol
+// nombres
+// apellidos
+// correo
+// nroRegistro
+// telefono
+// celular
+// direccion
+// fecha_nacimiento
+// contrasena
 
 const validate = values => {
   const errors = {}
-  if (!values.nombre) {
-    errors.nombre = 'Tienes que introducir tu nombre.'
-  } else if (values.nombre.length < 15) {
-    errors.nombre = 'Must be 15 characters or less'
+  if (!values.nroDocumento) {
+    errors.nroDocumento = 'Nro de documento obligatorio.'
+  } else if (values.nroDocumento.length <= 4) {
+    errors.nroDocumento = 'Por lo menos 4 caracteres.'
   }
-  if (!values.apellido) {
-    errors.apellido = 'Tienes que introducir tu apellido.'
-  } else if (values.apellido.length < 8) {
-    errors.apellido = 'Tiene que ser 8 characteres.'
+  if (!values.id_tipoDocumento) {
+    errors.id_tipoDocumento = 'Tipo de documento obligatorio.'
+  }
+
+  // Validación con ROL y ESPECIALIDAD.
+  if (!values.id_especialidad) {
+    errors.id_rol = 'Especialidad obligatorio.'
+  }
+
+  if (!values.id_rol) {
+    errors.id_rol = 'Rol obligatorio.'
+  }
+
+
+  if (!values.nombres) {
+    errors.nombres = 'Nombre completo es obligatorio.'
+  } else if (values.nombres.length <= 10) {
+    errors.nombres = 'Por lo menos 10 caracteres.'
+  }
+  if (!values.apellidos) {
+    errors.apellidos = 'Apellido completo obligatorio.'
+  } else if (values.apellidos.length < 10) {
+    errors.apellidos = 'Por lo menos 10 caracteres'
   }
   if (!values.correo) {
-    errors.correo = 'Tienes que introducir un correo.'
-  } else if (values.correo.length < 8) {
-    errors.correo = 'Tiene que ser por lo menos 8 characteres.'
+    errors.correo = 'Correo obligatorio.'
+  } else if (values.correo.length < 10) {
+    errors.correo = 'Como minimo 10 caracteres'
+  }
+  if (!values.nroRegistro) {
+    errors.nroRegistro = 'Nro de registro obligatorio.'
+  } else if (values.nroRegistro.length <= 4) {
+    errors.nroRegistro = 'Como minimo 4 caracteres'
+  }
+  // if (!values.telefono) {
+  //   errors.telefono = 'Tienes que introducir tu nombre.'
+  // } else if (values.telefono.length < 15) {
+  //   errors.telefono = 'Must be 15 characters or less'
+  // }
+  if (!values.celular) {
+    errors.celular = 'Nro de celular obligatorio.'
+  } else if (values.celular.length <= 7) {
+    errors.celular = 'Como minimo 7 caracteres.'
+  }
+  if (!values.direccion) {
+    errors.direccion = 'Dirección obligatorio.'
+  } else if (values.direccion.length < 10) {
+    errors.direccion = 'Como minimo 10 caracteres.'
+  }
+  if (!values.fecha_nacimiento) {
+    errors.fecha_nacimiento = 'Fecha de nacimiento obligatorio.'
   }
   if (!values.contrasena) {
-    errors.contrasena = 'Tienes que introducir una contraseña.'
-  } else if (values.contrasena.length < 7) {
-    errors.contrasena = 'Tiene que ser por lo menos 7 characteres.'
+    errors.contrasena = 'Contraseña obligatorio.'
+  } else if (values.contrasena.length <= 8) {
+    errors.contrasena = 'Como minimo 8 caracteres.'
   }
   
   return errors
@@ -43,12 +108,20 @@ const validate = values => {
 
 function mapStateToProps(state) {
 	return {
+    listaEspecialidades: state.especialidad.listar,
+    listaRoles: state.rol.listar,
 		registro: state.personal.registro
 	}
 }
 
 function mapDispatchToProps(dispatch) {
 	return {
+    listarEspecialidades: () => {
+      dispatch(listarEspecialidades())
+    },
+    listarRoles: () => {
+      dispatch(listarRoles())
+    },
 		registrarPersonal: (datosFormulario) => {
 			dispatch(registrarPersonal(datosFormulario))
 		}
