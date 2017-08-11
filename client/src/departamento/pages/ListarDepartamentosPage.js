1d243492f2227dda40f157d9e01aeb2cec387981
gamaliel <fabiogamaliel@gmail.com>
2017-08-11 04:05:40 -0400

    ListarDepartamentosPage component created

---
+++ client/src/departamento/pages/ListarDepartamentosPage.js
@@ -0,0 +1,11 @@
+import React , { Component } from 'react'
+
+import ListarContainer from '../components/Listar'
+
+class ListarDepartamentosPage extends Component {
+	render() {
+		return <ListarContainer/>
+	}
+}
+
+export default ListarDepartamentosPage
