13ae26a81ce04f99c7285db4ae32407509152bc0
gamaliel <fabiogamaliel@gmail.com>
2017-08-11 04:03:51 -0400

    Formulario component created

---
+++ client/src/departamento/components/Formulario/Formulario.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import { Field, reset } from 'redux-form'
+import ReactModal from 'react-modal'
+
+import Cargando from '../../../app/components/Cargando'
+
+import MensajeOerror from '../../../app/components/MensajeOerror'
+
+const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+  <div>
+    <div className="form-group">
+      <label htmlFor={label}>{ label }</label>
+      <input className="form-control" {...input} placeholder={label} type={type}/>
+    </div>
+    { touched && ((error && <label className="text-danger">{ error }</label>)) }
+  </div>
+)
+
+class Formulario extends Component {
+	constructor(props) {
+		super(props)
+		this.enviarFormulario = this.enviarFormulario.bind(this)
+		this.renderCargando = this.renderCargando.bind(this)
+	}
+
+	enviarFormulario(formProps) {
+		if(this.props.editarContenido) {
+			this.props.editarDepartamento(formProps)
+		} else {
+			this.props.crearDepartamento(formProps)
+		}
+	}
+
+	renderCargando(cargando) {
+		if(cargando) {
+			return <Cargando/>
+		} else {
+			return <span></span>
+		}
+	}
+
+	render() {
+		const customStyles = {
+		    content : {
+		  		height: '40vh',
+		  		position: 'none'
+		  	}
+		}
+
+		const { handleSubmit, pristine, reset, submitting } = this.props
+		
+		const { 
+			abirtoCrear, abirtoEditar, error, cargando, departamento 
+		} = this.props.formulario
+
+		let abierto = abirtoEditar ? abirtoEditar : abirtoCrear
+
+		if(abierto) {
+			return <ReactModal isOpen={abierto}
+					       	contentLabel="Minimal Modal Example"
+					       	style={customStyles}>
+
+				<div className='container'>
+					<div className='row'>
+						<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
+							<MensajeOerror error={error} mensaje={null}/>
+							{ this.renderCargando(cargando) }
+
+							<form onSubmit={handleSubmit(this.enviarFormulario)}>
+								<Field name='descripcion' type='text' component={renderField} label='Descripción'/>
+														
+								<button type="submit" className='btn btn-info btn-space' disabled={submitting}>Guardar</button>
+								<button type="button" onClick={ this.props.cerrarFormularioDepartamento } className='btn btn-primary btn-space'>Cancelar</button>
+							</form>
+						</div>
+					</div>
+				</div>
+
+			</ReactModal>
+		} else {
+			return <span></span>
+		}
+	}
+}
+
+export default Formulario
---
+++ client/src/departamento/components/Formulario/container.js
@@ -0,0 +1,53 @@
+import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
+
+import {
+	crearDepartamento,
+	editarDepartamento,
+	cerrarFormularioDepartamento
+} from '../../actions'
+
+import Formulario from './Formulario'
+
+const validate = (values) => {
+	const errors = {}
+
+	if(!values.descripcion) {
+		errors.descripcion = 'Tienes que introducir una descripción.'
+	}else if (values.descripcion.length < 5) {
+   		errors.descripcion = 'Tiene que ser por lo menos 5 characteres.'
+	}
+
+	return errors
+}
+
+function mapStateToProps(state) {
+	return {
+		formulario: state.departamento.formulario,
+		initialValues: state.departamento.formulario.departamento,
+		enableReinitialize: state.departamento.formulario.iniciarValores,
+		editarContenido: state.departamento.formulario.iniciarValores
+	}
+}
+
+
+function mapDispatchToProps(dispatch) {
+	return {
+		crearDepartamento: (datosFormulario) => {
+			dispatch(crearDepartamento(datosFormulario))
+		},
+		cerrarFormularioDepartamento: () => {
+			dispatch(cerrarFormularioDepartamento())
+		},
+		editarDepartamento: (datosFormulario) => {
+			dispatch(editarDepartamento(datosFormulario))
+		}
+	}
+}
+
+const form = reduxForm({
+	form: 'FormularioDepartamento',
+	validate
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(form(Formulario))
