4f3b592ca20f48a7bcc4710a4b7db55d9cc3e24b
gamaliel <fabiogamaliel@gmail.com>
2017-08-11 04:03:22 -0400

    departamentos actions created

---
+++ client/src/departamento/actions/index.js
@@ -0,0 +1,176 @@
+import {
+	ABRIR_FORMULARIO_CREAR_DEPARTAMENTO,
+
+	ABRIR_FORMULARIO_EDITAR_DEPARTAMENTO_REQUEST,
+	ABRIR_FORMULARIO_EDITAR_DEPARTAMENTO_EXITO,
+	ABRIR_FORMULARIO_EDITAR_DEPARTAMENTO_FALLO,
+
+	CERRAR_FORMULARIO_DEPARTAMENTO,
+
+	LISTAR_DEPARTAMENTOS_REQUEST,
+	LISTAR_DEPARTAMENTOS_EXITO,
+	LISTAR_DEPARTAMENTOS_FALLO,
+
+	// Create cuidad.
+	CREAR_DEPARTAMENTO_REQUEST,
+	CREAR_DEPARTAMENTO_EXITO,
+	CREAR_DEPARTAMENTO_FALLO,
+
+	// Show departamento.
+	MOSTRAR_DEPARTAMENTO_REQUEST,
+	MOSTRAR_DEPARTAMENTO_EXITO,
+	MOSTRAR_DEPARTAMENTO_FALLO,
+
+	CERRAR_MODAL_MOSTRAR_DEPARTAMENTO,
+
+	// Editar departamento.
+	EDITAR_DEPARTAMENTO_REQUEST,
+	EDITAR_DEPARTAMENTO_EXITO,
+	EDITAR_DEPARTAMENTO_FALLO,
+
+	// Delete departamento.
+	ELIMINAR_DEPARTAMENTO_REQUEST,
+	ELIMINAR_DEPARTAMENTO_EXITO,
+	ELIMINAR_DEPARTAMENTO_FALLO
+} from './types'
+
+import io from 'socket.io-client'
+import { socket } from '../../globalActions'
+
+import { browserHistory } from 'react-router'
+import { reset } from 'redux-form'
+
+export function abrirFormularioCrearDepartamento() {
+	return (dispatch) => {
+		dispatch(reset('FormularioDepartamento'))
+
+		dispatch({ type: ABRIR_FORMULARIO_CREAR_DEPARTAMENTO })
+	}
+}
+
+export function abrirFormularioEditarDepartamento(idDepartamento) {
+	return (dispatch) => {
+		dispatch({ type: ABRIR_FORMULARIO_EDITAR_DEPARTAMENTO_REQUEST })
+
+		socket.emit('mostrar_departamento', { id_departamento: idDepartamento })
+
+		socket.on('mostrar_departamento', (data) => {
+			// console.log(data)
+			if(data.error) {
+				dispatch({ type: ABRIR_FORMULARIO_EDITAR_DEPARTAMENTO_FALLO, payload: data.error })
+			} else {
+				dispatch({ type: ABRIR_FORMULARIO_EDITAR_DEPARTAMENTO_EXITO, payload: data })
+			}
+		})
+	}
+}
+
+export function cerrarFormularioDepartamento() {
+	return (dispatch) => {
+		dispatch({ type: CERRAR_FORMULARIO_DEPARTAMENTO })
+	}
+}
+
+export function listarDepartamentos() {
+	return (dispatch) => {
+
+		dispatch({ type: LISTAR_DEPARTAMENTOS_REQUEST })
+
+		var socket = io('http://localhost:3000')
+
+		socket.on('listar_departamentos', (data) => {
+
+			if(data.error) {
+				dispatch({ type: LISTAR_DEPARTAMENTOS_FALLO, payload: data.error })
+			} else {
+				dispatch({ type: LISTAR_DEPARTAMENTOS_EXITO, payload: data })
+			}
+		})
+	}
+}
+
+export function crearDepartamento(datosFormulario) {
+	return (dispatch) => {
+
+		dispatch({ type: CREAR_DEPARTAMENTO_REQUEST })
+
+		socket.emit('crear_departamento', datosFormulario)
+		socket.on('crear_departamento', (data) => {
+			if(data.err) {
+				dispatch({ type: CREAR_DEPARTAMENTO_FALLO, payload: data.error })
+			} else {
+				dispatch({ type: CREAR_DEPARTAMENTO_EXITO, payload: data })
+			}
+		})
+	
+		dispatch(reset('FormularioDepartamento'))
+	}
+}
+
+export function eliminarDepartamento(idDepartamento) {
+	return (dispatch) => {
+
+		dispatch({ type: ELIMINAR_DEPARTAMENTO_REQUEST })
+
+		socket.emit('eliminar_departamento', { id_departamento: idDepartamento })
+
+		socket.on('eliminar_departamento', (data) => {
+			if(data.error) {
+				dispatch({ type: ELIMINAR_DEPARTAMENTO_FALLO, payload: data.error })
+			} else {
+				dispatch({ type: ELIMINAR_DEPARTAMENTO_EXITO, payload: data })
+			}
+		})
+	}
+}
+
+
+export function mostrarDepartamento(idDepartamento) {
+	return (dispatch) => {
+		dispatch({ type: MOSTRAR_DEPARTAMENTO_REQUEST })
+
+		socket.emit('mostrar_departamento', { id_departamento: idDepartamento })
+
+		socket.on('mostrar_departamento', (data) => {
+			if(data.error) {
+				dispatch({ type: MOSTRAR_DEPARTAMENTO_FALLO, payload: data.error })
+			} else {
+				dispatch({ type: MOSTRAR_DEPARTAMENTO_EXITO, payload: data })
+			}
+		})
+	}
+}
+
+
+export function cerrarModalMostrarDepartamento() {
+	return (dispatch) => {
+		dispatch({ type: CERRAR_MODAL_MOSTRAR_DEPARTAMENTO })
+	}
+}
+
+export function editarDepartamento(datosFormulario) {
+	return (dispatch) => {
+
+		dispatch({ type: EDITAR_DEPARTAMENTO_REQUEST })
+
+		socket.emit('editar_departamento', datosFormulario)
+
+		socket.on('editar_departamento', (data) => {
+			if(data.error) {
+				dispatch({ type: EDITAR_DEPARTAMENTO_FALLO, payload: data.error })
+			} else {
+				dispatch({ type: EDITAR_DEPARTAMENTO_EXITO, payload: data })
+			}
+		})
+
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ client/src/departamento/actions/types.js
@@ -0,0 +1,36 @@
+// FIND.
+export const LISTAR_DEPARTAMENTOS_REQUEST = 'listar_departamentos_request'
+export const LISTAR_DEPARTAMENTOS_EXITO = 'listar_departamentos_exito'
+export const LISTAR_DEPARTAMENTOS_FALLO = 'listar_departamentos_fallo'
+
+// EDIT.
+export const ABRIR_FORMULARIO_EDITAR_DEPARTAMENTO_REQUEST = 'abrir_formulario_editar_departamento_request'
+export const ABRIR_FORMULARIO_EDITAR_DEPARTAMENTO_EXITO = 'abrir_formulario_editar_departamento_exito'
+export const ABRIR_FORMULARIO_EDITAR_DEPARTAMENTO_FALLO = 'abrir_formulario_editar_departamento_fallo'
+
+	export const EDITAR_DEPARTAMENTO_REQUEST = 'edit_departamento_request'
+	export const EDITAR_DEPARTAMENTO_EXITO = 'edit_departamento_exito'
+	export const EDITAR_DEPARTAMENTO_FALLO = 'edit_departamento_fallo'
+
+// CREATE.
+export const ABRIR_FORMULARIO_CREAR_DEPARTAMENTO = 'abrir_formulario_crear_departamento'
+
+	export const CREAR_DEPARTAMENTO_REQUEST = 'crear_departamento_request'
+	export const CREAR_DEPARTAMENTO_EXITO = 'crear_departamento_exito'
+	export const CREAR_DEPARTAMENTO_FALLO = 'crear_departamento_fallo'
+
+export const CERRAR_FORMULARIO_DEPARTAMENTO = 'cerrar_formulario_departamento'
+
+// SHOW.
+export const MOSTRAR_DEPARTAMENTO_REQUEST = 'mostrar_departamento_request'
+export const MOSTRAR_DEPARTAMENTO_EXITO = 'mostrar_departamento_exito'
+export const MOSTRAR_DEPARTAMENTO_FALLO = 'mostrar_departamento_fallo'
+
+export const CERRAR_MODAL_MOSTRAR_DEPARTAMENTO = 'cerrar_modal_mostrar_departamento'
+
+// DELETE.
+export const ELIMINAR_DEPARTAMENTO_REQUEST = 'eliminar_departamento_request'
+export const ELIMINAR_DEPARTAMENTO_EXITO = 'eliminar_departamento_exito'
+export const ELIMINAR_DEPARTAMENTO_FALLO = 'eliminar_departamento_fallo'
+
+
