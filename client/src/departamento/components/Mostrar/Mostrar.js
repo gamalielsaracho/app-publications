c2ca20d24f1b01ccba3452c846805d1662d87626
gamaliel <fabiogamaliel@gmail.com>
2017-08-11 04:04:41 -0400

    Mostrar component created

---
+++ client/src/departamento/components/Mostrar/Mostrar.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+
+import ReactModal from 'react-modal'
+
+import MensajeOerror from '../../../app/components/MensajeOerror'
+import Cargando from '../../../app/components/Cargando'
+
+class Mostrar extends Component {
+	constructor(props) {
+		super(props)
+		this.renderCargando = this.renderCargando.bind(this)
+		this.renderDepartamento = this.renderDepartamento.bind(this)
+	}
+
+	renderCargando(cargando) {
+		if(cargando) {
+			return <Cargando/>
+		} else {
+			return <span></span>
+		}
+	}
+
+	renderDepartamento(departamento) {
+		if(departamento) {
+			return <div>
+				<p><strong>Descripción:</strong> { departamento.descripcion }</p>
+			</div>
+		} else {
+			return <span></span>
+		}
+	}
+
+	render() {
+		const customStyles = {
+		    content : {
+		  		height: '40vh',
+		  		position: 'none'
+		  	}
+		}
+
+		const { cargando, departamento, error, abierto } = this.props.mostrar
+
+		console.log("Mostrar estÁ: "+this.props.mostrar.abierto)
+		
+		if(abierto) {
+			return <ReactModal isOpen={abierto}
+				       	contentLabel="Minimal Modal Example"
+				       	style={customStyles}>
+
+				<div className='container'>
+					
+					<div className='row end-lg end-md end-sm end-xs'>
+						<span className='icon-cross' onClick={() => { this.props.cerrarModalMostrarDepartamento() }}></span>
+					</div>
+
+					<div className='row'>
+						<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6 col-centered'>
+							{ this.renderCargando(cargando) }
+							<MensajeOerror error={error} mensaje={null}/>
+
+							{ this.renderDepartamento(departamento) }
+						</div>
+					</div>
+				</div>
+
+			</ReactModal>
+		} else {
+			return <span></span>
+		}
+
+	}
+}
+
+export default Mostrar
