c18517ec4cf62eac776478fd1cd8e6c56e4b4cd2
gamaliel <fabiogamaliel@gmail.com>
2017-08-12 04:31:19 -0400

    MenuContainer called

--- client/src/dashboard/components/DashBoard/Dashboard.js
+++ client/src/dashboard/components/DashBoard/Dashboard.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
-import Menu from '../Menu'
+import MenuContainer from '../Menu'
 
 class DashBoard extends Component {
 	render() {
 
 		return <div className='container-fluid'>
 			<div className='row'>
-				<Menu/>
+				<MenuContainer/>
 
 				<div className='col-xs-12 col-sm-12 col-md-10 col-lg-10'>
 					<br/>
