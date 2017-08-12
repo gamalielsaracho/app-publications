10a41e580cfe6c8536bbda06ca173e8d7e938e54
gamaliel <fabiogamaliel@gmail.com>
2017-08-12 04:34:38 -0400

    listarDepartamentos function called

--- client/src/ciudad/components/Formulario/container.js
+++ client/src/ciudad/components/Formulario/container.js
@@ -4,8 +4,12 @@ import { Field, reduxForm } from 'redux-form'
 import {
 	crearCiudad,
 	editarCiudad,
-	cerrarFormularioCiudad
+	cerrarFormularioCiudad,
 } from '../../actions'
+
+import {
+	listarDepartamentos
+} from '../../../departamento/actions'
 
 import Formulario from './Formulario'
 
@@ -26,7 +30,9 @@ function mapStateToProps(state) {
 		formulario: state.ciudad.formulario,
 		initialValues: state.ciudad.formulario.ciudad,
 		enableReinitialize: state.ciudad.formulario.iniciarValores,
-		editarContenido: state.ciudad.formulario.iniciarValores
+		editarContenido: state.ciudad.formulario.iniciarValores,
+
+		listaDepartamentos: state.departamento.listar
 	}
 }
 
@@ -41,6 +47,11 @@ function mapDispatchToProps(dispatch) {
 		},
 		editarCiudad: (datosFormulario) => {
 			dispatch(editarCiudad(datosFormulario))
+		},
+
+
+		listarDepartamentos: () => {
+			dispatch(listarDepartamentos())
 		}
 	}
 }
