e6cbe2395f556bf75936349477996eb95f58bdaa
gamaliel <fabiogamaliel@gmail.com>
2017-08-13 05:22:38 -0400

    alergia sockets added

---
+++ server/app/alergia/alergia.sockets.js
@@ -0,0 +1,73 @@
+import Alergia from './alergia.model'
+
+export default (socket, io) => {
+	
+		function alergias() {
+			Alergia.find((err, alergias) => {
+				if(err) {
+					console.log(err)
+				
+					socket.emit('listar_alergias', { error: 'Lo sentimos, acurrió un error. intente más tarde.' })
+					return
+				}
+
+				io.sockets.emit('listar_alergias', { alergias: alergias })
+			})
+		}
+	
+		alergias()
+
+
+		socket.on('crear_alergia', function(data) {
+			Alergia.create(data, (err, alergia) => {
+				if(err) {
+					socket.emit('crear_alergia', { error: 'Ocurrió un error, intente más tarde.' })
+					return
+				}
+
+				socket.emit('crear_alergia', { mensaje: 'Se agregó exitósamente.' })
+			
+				alergias()
+			})
+		})
+
+
+		socket.on('mostrar_alergia', (data) => {
+			Alergia.findById(data.id_alergia, (err, alergia) => {
+				if(err) {
+					socket.emit('mostrar_alergia', { error: 'Ocurrió un error, intente más tarde.' })
+					return
+				}
+
+				socket.emit('mostrar_alergia', alergia[0])
+			})
+		})
+
+
+		socket.on('eliminar_alergia', (data) => {
+			Alergia.delete(data.id_alergia, (err) => {
+				if(err) {
+					socket.emit('eliminar_alergia', { error: 'Ocurrió un error, intente más tarde.' })
+					return
+				}
+
+				socket.emit('eliminar_alergia', { mensaje: 'Se Eliminó exitósamente.' })
+
+				alergias()
+			})
+		})
+
+
+		socket.on('editar_alergia', (data) => {
+			Alergia.update(data, (err) => {
+				if(err) {
+					socket.emit('editar_alergia', { error: 'Ocurrió un error, intente más tarde.' })
+					return
+				}
+
+				socket.emit('editar_alergia', { mensaje: 'Se actualizó exitósamente.' })
+			
+				alergias()
+			})
+		})
+}
