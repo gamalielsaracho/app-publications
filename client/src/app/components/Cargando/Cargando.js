81762b528868af20b4278ccfef50c51c188bf253
gamaliel <fabiogamaliel@gmail.com>
2017-08-13 05:27:01 -0400

    spinner structure added

--- client/src/app/components/Cargando/Cargando.js
+++ client/src/app/components/Cargando/Cargando.js
@@ -6,31 +6,14 @@ class Cargando extends Component {
 	}
 
 	render() {
-		return <div className='row center-lg center-md center-sm center-xs'>
-				<div className='col-xs-12 col-sm-8 col-md-6 col-lg-6'>
-					<div className="preloader-wrapper big active">
-					    <div className="spinner-layer spinner-blue">
-					    	<div className="circle-clipper left">
-					      		<div className="circle"></div>
-					    	</div><div className="gap-patch">
-					      		<div className="circle"></div>
-					    	</div><div className="circle-clipper right">
-					      		<div className="circle"></div>
-					    	</div>
-					    </div>
-
-					    <div className="spinner-layer spinner-red">
-						    <div className="circle-clipper left">
-						    	<div className="circle"></div>
-						    </div><div className="gap-patch">
-						    	<div className="circle"></div>
-						    </div><div className="circle-clipper right">
-						    	<div className="circle"></div>
-						    </div>
-					    </div>
-					</div>
+		return <div className='row'>
+			<div className='col-xs-4 col-sm-4 col-md-4 col-lg-4 col-centered'>
+				<div className="spinner">
+					<div className="cube1"></div>
+					<div className="cube2"></div>
 				</div>
 			</div>
+		</div>
 	}
 }
 
