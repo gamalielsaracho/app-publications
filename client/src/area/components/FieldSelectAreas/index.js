21993520dac32fea0a8977478436fdcdaea6a104
gamaliel <fabiogamaliel@gmail.com>
2017-08-16 16:53:32 -0400

    FieldSelectAreas added

---
+++ client/src/area/components/FieldSelectAreas/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function FieldSelectAreas({ input, label, listaAreas, type, meta: { touched, error, warning } }) {
+	if(listaAreas.areas) {
+		return <div>
+			<div className='form-group'>
+				<label htmlFor={label}>{label}</label>
+				<select {...input} name={name} className='form-control'>
+					<option value=''>Selecionar area</option>
+					{
+						listaAreas.areas.map((area) => {
+							return <option key={area.id_area} value={area.id_area}>
+								{ area.descripcion }
+							</option>
+						})
+					}
+				</select>
+			</div>
+		   	{ touched && ((error && <p className="text-danger text-center">{ error }</p>)) }
+		</div>
+	} else {
+		return <span></span>
+	}
+}
+
+export default FieldSelectAreas
