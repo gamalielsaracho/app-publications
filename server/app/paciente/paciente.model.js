f04c973fd1a2f2584e51be834a0ab297abde16d1
gamaliel <fabiogamaliel@gmail.com>
2017-08-18 19:37:00 -0400

    some functions deleted in paciente model

--- server/app/paciente/paciente.model.js
+++ server/app/paciente/paciente.model.js
@@ -58,22 +58,22 @@ exports.delete = (data, callback) => {
 
 // Esta función trae todas las alergias que tiene un paciente.
 // listar por id_paciente.
-exports.findAlergiasByPaciente = (nroDocumento, id_tipoDocumento, callback) => {
-	return connection.query('select * from alergias al pacientesAlergias paAl WHERE paAl.nroDocumento = ? AND paAl.id_tipoDocumento = ? AND al.id_alergia = paAl.id_alergia', [nroDocumento, id_tipoDocumento], callback)
+// exports.findAlergiasByPaciente = (nroDocumento, id_tipoDocumento, callback) => {
+// 	return connection.query('select * from alergias al pacientesAlergias paAl WHERE paAl.nroDocumento = ? AND paAl.id_tipoDocumento = ? AND al.id_alergia = paAl.id_alergia', [nroDocumento, id_tipoDocumento], callback)
 
-	connection.end()
-}
+// 	connection.end()
+// }
 
-// En data tendrá, nroDocumento, id_tipoDocumento, id_alergia, observaciones.
-exports.addAlergiaByPaciente = (data, callback) => {
-	return connection.query('INSERT INTO pacientesAlergias SET ?', data, callback)
+// // En data tendrá, nroDocumento, id_tipoDocumento, id_alergia, observaciones.
+// exports.addAlergiaByPaciente = (data, callback) => {
+// 	return connection.query('INSERT INTO pacientesAlergias SET ?', data, callback)
 
-	connection.end()
-}
+// 	connection.end()
+// }
 
-// Eliminar 
-exports.deleteAlergiaByPaciente = (data, callback) => {
-	return connection.query('DELETE FROM pacientesAlergias WHERE nroDocumento = ?, id_tipoDocumento = ?, id_alergia = ?', [data.nroDocumento, data.id_tipoDocumento, data.id_alergia], callback)
+// // Eliminar 
+// exports.deleteAlergiaByPaciente = (data, callback) => {
+// 	return connection.query('DELETE FROM pacientesAlergias WHERE nroDocumento = ?, id_tipoDocumento = ?, id_alergia = ?', [data.nroDocumento, data.id_tipoDocumento, data.id_alergia], callback)
 
-	connection.end()
-}
+// 	connection.end()
+// }
