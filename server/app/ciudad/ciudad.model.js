ce3049d01a814c3be7efe927766b34a774ef53a4
gamaliel <fabiogamaliel@gmail.com>
2017-08-24 15:49:05 -0400

    functions deleted

--- server/app/ciudad/ciudad.model.js
+++ server/app/ciudad/ciudad.model.js
@@ -29,15 +29,3 @@ exports.delete = (idCiudad, callback) => {
 
 	connection.end()
 }
-
-exports.create = (data, callback) => {
-	return connection.query('INSERT INTO ciudades SET ?', data, callback)
-
-	connection.end()
-}
-
-exports.update = (data, callback) => {
-	return connection.query('update ciudades set descripcion = ? where id_ciudad = ?', [data.descripcion, data.id_ciudad], callback)
-
-	connection.end()
-}
