1b3d66a5140e726b6c74625f1a5801405402ee52
gamaliel <fabiogamaliel@gmail.com>
2017-08-26 19:26:21 -0400

    ListarCitasPage page created

---
+++ client/src/cita/pages/ListarCitasPage.js
@@ -0,0 +1,11 @@
+import React , { Component } from 'react'
+
+import ListarCitasContainer from '../components/Listar'
+
+class ListarCitasPage extends Component {
+	render() {
+		return <ListarCitasContainer/>
+	}
+}
+
+export default ListarCitasPage
