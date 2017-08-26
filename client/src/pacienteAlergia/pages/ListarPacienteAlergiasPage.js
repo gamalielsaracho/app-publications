be971736e11b4a8f7796c21e85cfa502fa341689
gamaliel <fabiogamaliel@gmail.com>
2017-08-25 20:44:49 -0400

    nroDocumento idTipoDocumento properties deleted

--- client/src/pacienteAlergia/pages/ListarPacienteAlergiasPage.js
+++ client/src/pacienteAlergia/pages/ListarPacienteAlergiasPage.js
@@ -5,8 +5,7 @@ import ListarPacienteAlergiasContainer from '././../components/Listar'
 class ListarPacienteAlergiasPage extends Component {
 	render() {
 		return <ListarPacienteAlergiasContainer
-				nroDocumento={this.props.params.nroDocumento}
-				idTipoDocumento={this.props.params.idTipoDocumento}/>
+				idPaciente={this.props.params.idPaciente}/>
 	}
 }
 
