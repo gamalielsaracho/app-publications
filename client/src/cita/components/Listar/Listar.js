7ba6ab8fc9756bf3d85dcacfd25ace0c6ddf88df
gamaliel <fabiogamaliel@gmail.com>
2017-08-29 11:57:46 -0400

    renderOptionsByRol  and renderOptionsByRol

--- client/src/cita/components/Listar/Listar.js
+++ client/src/cita/components/Listar/Listar.js
@@ -10,6 +10,8 @@ class Listar extends Component {
 	constructor(props) {
 		super(props)
 		this.renderCitas = this.renderCitas.bind(this)
+		this.renderOptionsByRol = this.renderOptionsByRol.bind(this)
+		this.renderAddButton = this.renderAddButton.bind(this)
 	}
 
 	componentWillMount() {
@@ -24,6 +26,58 @@ class Listar extends Component {
 		}
 	}	
 
+	renderOptionsByRol(rol, cita) {
+		// let disabled = true
+		// let mostrarOpcion = false
+		// let editarOpcion = false
+		// let eliminarOpcion = false
+
+		// if(rol == 'medico' || rol == 'enfermeria') {
+		// 	editarOpcion = true
+		// 	eliminarOpcion = true
+		// }else {
+		// 	mostrarOpcion = false
+		// 	editarOpcion = false
+		// 	eliminarOpcion = false
+		// }
+
+		switch(rol) {
+			case 'administracion':
+				return <div>
+					<button type="button" onClick={() => { this.props.mostrarCita(cita.id_cita) }} className="btn btn-info btn-space">Mostrar</button>
+					<button type="button" onClick={() => { this.props.abrirFormularioEditarCita(cita.id_cita) }} className="btn btn-warning btn-space">Editar</button>
+					<button type="button" onClick={() => { this.props.eliminarCita(cita.id_cita) }} className="btn btn-danger btn-space">Eliminar</button>
+				</div>
+			case 'ventanilla':
+				return <div>
+					<button type="button" onClick={() => { this.props.mostrarCita(cita.id_cita) }} className="btn btn-info btn-space">Mostrar</button>
+					<button type="button" onClick={() => { this.props.abrirFormularioEditarCita(cita.id_cita) }} className="btn btn-warning btn-space">Editar</button>
+					<button type="button" onClick={() => { this.props.eliminarCita(cita.id_cita) }} className="btn btn-danger btn-space">Eliminar</button>
+				</div>
+			case 'medico':
+				return <div>
+					<button type="button" onClick={() => { this.props.mostrarCita(cita.id_cita) }} className="btn btn-info btn-space">Mostrar</button>
+				</div>
+			case 'enfermeria':
+				return <div>
+					<button type="button" onClick={() => { this.props.mostrarCita(cita.id_cita) }} className="btn btn-info btn-space">Mostrar</button>
+				</div>
+		}
+
+	}
+
+	renderAddButton(rol) {
+		if(rol == 'ventanilla') {
+			return <div className='row'>
+				<div className='col-xs-12 col-sm-8 col-md-6 col-lg-4'>
+					<button onClick={ this.props.abrirFormularioCrearCita } className='btn btn-success'>Agregar</button>
+				</div>
+			</div>
+		} else {
+			return false
+		}
+	}
+
 	renderCitas(citas) {
 		// console.log(citas)
 
@@ -36,9 +90,7 @@ class Listar extends Component {
 			            <td>{ cita.hora }</td>
 			            <td>{ cita.pendiente }</td>
 			            <td>
-							<button type="button" onClick={() => { this.props.mostrarCita(cita.id_cita) }} className="btn btn-info btn-space">Mostrar</button>
-							<button type="button" onClick={() => { this.props.abrirFormularioEditarCita(cita.id_cita) }} className="btn btn-warning btn-space">Editar</button>
-							<button type="button" onClick={() => { this.props.eliminarCita(cita.id_cita) }} className="btn btn-danger btn-space">Eliminar</button>
+			            	{ this.renderOptionsByRol('enfermeria', cita) }
 			            </td>
 			        </tr>		
 				})
@@ -63,11 +115,8 @@ class Listar extends Component {
 
 					<MensajeOerror error={error} mensaje={null}/>
 
-					<div className='row'>
-						<div className='col-xs-12 col-sm-8 col-md-6 col-lg-4'>
-							<button onClick={ this.props.abrirFormularioCrearCita } className='btn btn-success'>Agregar</button>
-						</div>
-					</div>
+					{ this.renderAddButton('enfermeria') }
+
 					<br/>
 
 					<div className='table-responsive'>
