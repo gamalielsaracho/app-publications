fbdbf76da7e2c3eb06aea6f7b0af4865ad3b425d
gamaliel <fabiogamaliel@gmail.com>
2017-08-29 11:58:06 -0400

    className added

--- client/src/cita/components/Formulario/Formulario.js
+++ client/src/cita/components/Formulario/Formulario.js
@@ -13,12 +13,12 @@ import FieldSelectEspecialidadesContainer from '../../../especialidades/componen
 
 import FieldSelectPesonales from '../../../usuario/components/FieldSelectPesonales'
 
-const renderField = ({ input, nombreNodo, label, type, meta: { touched, error, warning } }) => (
-  
+const renderField = ({ input, nombreNodo, label, type, meta: { touched, error, warning } }) => (  
   <div>
 	<div className="form-group">
+		{ console.log(nombreNodo) }
 	 	<label htmlFor={label}>{label}</label>
-    	<input className={nombreNodo} {...input} placeholder={label} type={type}/>
+    	<input className='form-control' disabled className={nombreNodo+' form-control'} {...input} placeholder={label} type={type}/>
 	</div>
     { touched && ((error && <label className="text-danger">{ error }</label>)) }
   </div>
@@ -39,11 +39,11 @@ class Formulario extends Component {
 	enviarFormulario(formProps) {		
 		console.log(formProps)
 
-		if(this.props.editarContenido) {
-			this.props.editarCita(formProps)
-		} else {
-			this.props.crearCita(formProps)
-		}
+		// if(this.props.editarContenido) {
+		// 	this.props.editarCita(formProps)
+		// } else {
+		// 	this.props.crearCita(formProps)
+		// }
 	}
 
 	renderCargando(cargando) {
@@ -90,26 +90,8 @@ class Formulario extends Component {
 
 						<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
 
-							<CalendarioCitaContainer citas= {[
-								{
-				                    title: 'Pedro Raul',
-				                    start: '2017-08-21T08:00:00',
-				                    end: '2017-08-21T08:30:00',
-				                    allDay: false
-				                },
-				                {
-				                    title: 'Rie Motomori',
-				                    start: '2017-08-23',
-				                    // end: new Date(y, m, 1, 9, 00),
-				                    allDay: false
-				                },
-				                {
-				                    title: 'Gamaliel Saracho',
-				                    start: '2017-08-23T08:00:00',
-				                    end: '2017-08-23T08:30:00',
-				                    allDay: false
-				                }
-							]}/>
+							<CalendarioCitaContainer citas = {this.props.listar.citas}
+								valoresFiltro={this.props.valoresFiltro}/>
 							
 						</div>
 						<div className='col-xs-12 col-sm-6 col-md-6 col-lg-4'>
@@ -117,12 +99,21 @@ class Formulario extends Component {
 								<Field name='id_especialidad' type='text' component={FieldSelectEspecialidadesContainer} listaEspecialidades={this.props.listaEspecialidades} label='Especialidad'/>
 								
 								<Field name='id_personal' valoresFiltro={this.props.valoresFiltro} type='text' component={FieldSelectPesonales} listaPesonales={this.props.listaPesonales} label='Profesionales'/>
-								<h3>Fecha: <strong className='fechaNodo'></strong></h3>
-								<h3>Hora: <strong className='horaInicioNodo'></strong></h3>
 								
+								<div className='row'>
+									<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
+										<Field nombreNodo='fechaNodo' name='fecha' type='text' component={renderField} label='Fecha'/>
+									</div>
+									<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
+										<Field nombreNodo='horaInicioNodo' name='inicio' type='text' component={renderField} label='Hora inicio'/>
+										<Field nombreNodo='horaFinNodo' name='fin' type='text' component={renderField} label='Hora fin'/>
+									</div>
+								</div>
+
 								
 								<h1>Especialidad: { this.props.valoresFiltro.id_especialidad }</h1>
 								<h1>Médico/a: { this.props.valoresFiltro.id_personal }</h1>
+								
 								<button type="submit" className="btn btn-info btn-space" disabled={pristine || submitting}>Guardar</button>
 								<button type="button" onClick={ this.props.cerrarFormularioCita } className="btn btn-primary btn-space">Cancelar</button>
 							</form>
@@ -136,10 +127,9 @@ class Formulario extends Component {
 			return <span></span>
 		}
 														
+								// <h3>Fecha: <strong className='fechaNodo'></strong></h3>
+								// <h3>Hora: <strong className='horaInicioNodo'></strong></h3>
 	}
-								// <Field nombreNodo='fechaNodo' name='fecha' type='text' component={renderField} label='Fecha'/>
-								// <Field nombreNodo='horaInicioNodo' name='inicio' type='text' component={renderField} label='Hora inicio'/>
-								// <Field nombreNodo='horaFinNodo' name='fin' type='text' component={renderField} label='Hora fin'/>
 }
 
 export default Formulario
