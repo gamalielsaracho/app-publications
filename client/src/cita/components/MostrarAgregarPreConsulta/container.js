5c13e5b4180c5a6e897a0a2d7419b7ef73465f70
gamaliel <fabiogamaliel@gmail.com>
2017-09-03 05:02:43 -0400

    MostrarAgregarPreConsulta component created

---
+++ client/src/cita/components/MostrarAgregarPreConsulta/container.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux'
+
+import {
+} from '../../actions'
+
+import MostrarAgregarPreConsulta from './MostrarAgregarPreConsulta'
+
+function mapStateToProps(state) {
+	return {
+		mostrarCitaAgregarPreConsulta: state.cita.mostrarCitaAgregarPreConsulta
+	}
+}
+
+
+function mapDispatchToProps(dispatch) {
+	return {
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MostrarAgregarPreConsulta)
+
+
---
+++ client/src/cita/components/MostrarAgregarPreConsulta/index.js
@@ -0,0 +1,2 @@
+import MostrarAgregarPreConsultaContainer from './container'
+export default MostrarAgregarPreConsultaContainer
