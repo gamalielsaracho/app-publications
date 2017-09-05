f46eabe37bcf61fcc398575a92288a1ed5bd0f1c
gamaliel <fabiogamaliel@gmail.com>
2017-09-05 15:40:13 -0400

    conditional added

--- client/src/preconsulta/components/Mostrar/Mostrar.js
+++ client/src/preconsulta/components/Mostrar/Mostrar.js
@@ -5,6 +5,9 @@ import ReactModal from 'react-modal'
 import MensajeOerror from '../../../app/components/MensajeOerror'
 import Cargando from '../../../app/components/Cargando'
 
+// PreConsulta X Parametro.
+import FormularioPreConsultaParametroContainer from '../../../preConsultaParametro/components/Formulario'
+
 class Mostrar extends Component {
 	constructor(props) {
 		super(props)
@@ -25,9 +28,17 @@ class Mostrar extends Component {
 	}
 
 	renderPreConsulta(preConsulta) {
-		if(preConsulta) {
-			return <div>
-				<p><strong>Nombre:</strong> { preConsulta.fecha }</p>
+		if(preConsulta && preConsulta.personal !== undefined) {
+			return <div className='row'>
+				<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
+					<p><strong>Enfermero/a:</strong>{ preConsulta.personal.nombres +' '+ preConsulta.personal.apellidos }</p>
+					<p><strong>Fecha:</strong>{ preConsulta.preconsulta.fecha }</p>
+					<p><strong>Nivel:</strong>{ preConsulta.nivel.descripcion }</p>				
+				</div>
+				<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
+					<FormularioPreConsultaParametroContainer
+						idPreConsulta={this.props.idPreConsulta}/>
+				</div>
 			</div>
 		} else {
 			return <span></span>
@@ -38,17 +49,13 @@ class Mostrar extends Component {
 
 		const { cargando, preConsulta, error, abierto } = this.props.mostrar
 
-		console.log(this.props.mostrar)
-
-		console.log("Mostrar estÁ: "+this.props.mostrar.abierto)
+		// console.log(this.props.mostrar)
 		
-		return <div className='row'>
-			<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6 col-centered'>
-				{ this.renderCargando(cargando) }
-				<MensajeOerror error={error} mensaje={null}/>
+		return <div>
+			{ this.renderCargando(cargando) }
+			<MensajeOerror error={error} mensaje={null}/>
 
-				{ this.renderPreConsulta(preConsulta) }
-			</div>
+			{ this.renderPreConsulta(preConsulta) }
 		</div>
 
 	}
