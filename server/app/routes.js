434a52db2888f6ae45088d24ba3c295b9d046943
gamaliel <fabiogamaliel@gmail.com>
2017-09-12 16:46:40 -0400

    preConsultaParametroRoutes called

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,8 +1,10 @@
-import usuarioRoutes from './usuario/usuario.routes.js'
-import rolRoutes from './rol/rol.routes.js'
+// import usuarioRoutes from './personal/personal.routes.js'
+// import rolRoutes from './rol/rol.routes.js'
 
+import preConsultaParametroRoutes from './preConsultaParametro/preConsultaParametro.routes'
 
 export default (app) => {
-	usuarioRoutes(app)
-	rolRoutes(app)
+	// usuarioRoutes(app)
+	// rolRoutes(app)
+	preConsultaParametroRoutes(app)
 }
