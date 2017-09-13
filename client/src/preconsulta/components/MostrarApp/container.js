66bc89d227dd1a14199d650a0af0d6b6e5313abf
gamaliel <fabiogamaliel@gmail.com>
2017-09-13 15:01:51 -0400

    listarConsultas actions added

--- client/src/preconsulta/components/MostrarApp/container.js
+++ client/src/preconsulta/components/MostrarApp/container.js
@@ -2,6 +2,10 @@ import { connect } from 'react-redux'
 
 import MostrarApp from './MostrarApp'
 
+import {
+	listarConsultas
+} from '../../../consulta/actions'
+
 function mapStateToProps(state, ownProps) {
 	return {
 		idPreConsulta: ownProps.params.idPreConsulta,
@@ -9,13 +13,19 @@ function mapStateToProps(state, ownProps) {
 		// obtenemos los datos de la cita que se está mostrando
 		// para pasarle id_preconsulta al menu en la url para mostrar
 		// la preconsulta.
-		cita: state.cita.mostrar.cita
+		cita: state.cita.mostrar.cita,
+
+		// Obtener las consultas para filtarla por id_preConsulta y id_personal
+		// para mostrarlo dentro del menu y pasarle el id_consulta.
+		listar: state.consulta.listar
 	}
 }
 
 function mapDispatchToProps(dispatch) {
 	return {
-		
+		listarConsultas: () => {
+			dispatch(listarConsultas())
+		}
 	}
 }
 
