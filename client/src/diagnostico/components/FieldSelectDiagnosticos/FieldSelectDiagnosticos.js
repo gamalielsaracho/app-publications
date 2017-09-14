9c6f62650b79cdc33001e87542752c523ec8f13c
gamaliel <fabiogamaliel@gmail.com>
2017-09-14 13:30:23 -0400

    listaDiagnosticos object added

--- client/src/diagnostico/components/FieldSelectDiagnosticos/FieldSelectDiagnosticos.js
+++ client/src/diagnostico/components/FieldSelectDiagnosticos/FieldSelectDiagnosticos.js
@@ -1,42 +1,41 @@
 import React, { Component } from 'react'
 
+import Cargando from '../../../app/components/Cargando'
+
 class FieldSelectDiagnosticos extends Component {
 	render() {
-		const { input, label, listar, type, meta: { touched, error, warning } } = this.props
+		const { input, label, listaDiagnosticos, type, meta: { touched, error, warning } } = this.props
 
-		if(listar.cargando) {
-			return <p>Cargando..</p>
-		} else {
-			if(listar.diagnosticos) {
-				return <div className='form-group'>
-					<label htmlFor={label}>{label}</label>
+		let diagnosticos = listaDiagnosticos.diagnosticos
+
+		if(diagnosticos) {
+			return <div className='form-group'>
+				<label htmlFor={label}>{label}</label>
 					
-					<div className='form-inline'>
-						<div className='form-group'>
-							<select {...input} name={name} className='form-control'>
-								<option value=''>Selecionar nivel</option>
-								{
-									listar.diagnosticos.map((diagnostico) => {
-										return <option key={diagnostico.id_diagnostico} value={nivel.id_diagnostico}>
-											{ diagnostico.descripcion }
-										</option>
-									})
-								}
-							</select>
-						</div>
-
-						<button type="button" onClick={this.props.abrirFormularioCrearDiagnostico} className="btn btn-success btn-space btn-sm">
-							<span className="glyphicon glyphicon-plus" aria-hidden="true"></span> Nuevo nivel
-						</button>
-
-				    	{ touched && ((error && <div><br/><label className="text-danger">{ error }</label></div>)) }
+				<div className='form-inline'>
+					<div className='form-group'>
+						<select {...input} name={name} className='form-control'>
+							<option value=''>Seleccionar diagnóstico</option>
+							{
+								diagnosticos.map((diagnostico) => {
+									return <option key={diagnostico.id_diagnostico} value={diagnostico.id_diagnostico}>
+										{ diagnostico.descripcion }
+									</option>
+								})
+							}
+						</select>
 					</div>
-				</div>
-			} else {
-				return <span></span>
-			}
-		}
 
+					<button type="button" onClick={this.props.abrirFormularioCrearDiagnostico} className="btn btn-success btn-space btn-sm">
+						<span className="glyphicon glyphicon-plus" aria-hidden="true"></span> Nuevo diagnóstico
+					</button>
+
+				    { touched && ((error && <div><br/><label className="text-danger">{ error }</label></div>)) }
+				</div>
+			</div>
+		} else {
+			return <span></span>
+		}
 	}
 }
 
