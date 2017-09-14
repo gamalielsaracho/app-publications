ce96992bedcda26bd4e28c799e17edc038b04637
gamaliel <fabiogamaliel@gmail.com>
2017-09-14 13:33:30 -0400

    consulta model modified

--- server/app/consulta/consulta.model.js
+++ server/app/consulta/consulta.model.js
@@ -7,10 +7,12 @@ exports.find = (callback) => {
 			FROM
 				consultas consulta,
 				diagnosticos diagnostico,
-				personales personal
+				personales personal,
+				pacientes paciente
 			WHERE
 				consulta.id_personal = personal.id_personal AND
-				consulta.id_diagnostico = diagnostico.id_diagnostico
+				consulta.id_diagnostico = diagnostico.id_diagnostico AND
+				consulta.id_paciente = paciente.id_paciente
 	`
 
 	var options = {
@@ -23,8 +25,116 @@ exports.find = (callback) => {
 	connection.end()
 }
 
-exports.findById = (data, callback) => {
+// Listar todas las consultas que realizó un médico/a.
+// para mostrarlo al mismo.
+exports.findListByIdPersonal = (idPersonal, callback) => {
+	let q = `
+		SELECT * 
+			FROM
+				consultas consulta,
+				diagnosticos diagnostico,
+				personales personal,
+				pacientes paciente
+			WHERE
+				consulta.id_personal = personal.id_personal AND
+				consulta.id_diagnostico = diagnostico.id_diagnostico AND
+				consulta.id_paciente = paciente.id_paciente AND
+				consulta.id_personal = ?
+	`
 
+	var options = {
+		sql: q, 
+		nestTables: true
+	}
+
+	return connection.query(options, [idPersonal], callback)
+
+	connection.end()
+}
+
+// Para el historial clinico del paciente.
+exports.findListByIdPaciente = (idPaciente, callback) => {
+
+	let q = `
+		SELECT * 
+			FROM
+				consultas consulta,
+				diagnosticos diagnostico,
+				personales personal,
+				pacientes paciente
+			WHERE
+				consulta.id_personal = personal.id_personal AND
+				consulta.id_diagnostico = diagnostico.id_diagnostico AND
+				consulta.id_paciente = paciente.id_paciente AND
+				consulta.id_paciente = ?
+	`
+
+	var options = {
+		sql: q, 
+		nestTables: true
+	}
+
+	return connection.query(options, [idPaciente], callback)
+
+	connection.end()
+}
+
+exports.findById = (data, callback) => {
+	let q = `
+		SELECT * 
+			FROM 
+				consultas consulta,
+				diagnosticos diagnostico,
+				personales personal
+			WHERE
+				consulta.id_personal = personal.id_personal AND
+				consulta.id_diagnostico = diagnostico.id_diagnostico AND
+				id_consulta = ?
+	`
+
+	var options = {
+		sql: q,
+		nestTables: true
+	}
+
+	return connection.query(options, [data.id_consulta], callback)
+
+	connection.end()
+}
+
+// exports.verifyIfExist = (data, callback) => {
+// 	let q = `
+// 		SELECT * FROM consultas 
+// 			WHERE
+// 			id_diagnostico = ?
+// 	`
+
+// 	return connection.query(q, [data.id_diagnostico], callback)
+
+// 	connection.end()
+// }
+
+exports.create = (data, callback) => {
+	let q = `
+		INSERT INTO consultas (id_consulta, fecha, fechaProximaConsulta, 
+		id_personal, id_diagnostico, observacionDiagnostico, id_preconsulta,
+		id_paciente)
+
+			VALUES (null, ?, ?, ?, ?, LOWER(?), ?, ?);
+	`
+
+	return connection.query(q, [ data.fecha,
+								 data.fechaProximaConsulta,
+								 data.id_personal,
+								 data.id_diagnostico,
+								 data.observacionDiagnostico.trim(), 
+								 data.id_preconsulta,
+								 data.id_paciente ], callback)
+
+	connection.end()
+}
+
+exports.findByIdToUpdate = (data, callback) => {
 	let q = `
 		SELECT * FROM consultas 
 			WHERE
@@ -41,76 +151,19 @@ exports.findById = (data, callback) => {
 	connection.end()
 }
 
-exports.verifyIfExist = (data, callback) => {
-	let q = `
-		SELECT * FROM consultas 
-			WHERE
-			id_preconsulta = ? AND
-			id_personal = ? AND
-			id_diagnostico = ?
-	`
-
-	return connection.query(q, [data.id_preconsulta, data.id_personal, data.id_diagnostico], callback)
-
-	connection.end()
-}
-
-exports.create = (data, callback) => {
-	let q = `
-		INSERT INTO consultas (id_consulta, fecha, fechaPoximaConsulta, 
-		id_personal, id_diagnostico, observacionDiagnostico, id_preconsulta)
-
-			VALUES (null, ?, ?, ?, ?, LOWER(?), ?);
-	`
-
-	return connection.query(q, [ data.fecha,
-								 data.fechaPoximaConsulta,
-								 data.id_personal,
-								 data.id_diagnostico,
-								 data.observacionDiagnostico.trim(), 
-								 data.id_preconsulta
-								 ], callback)
-
-	connection.end()
-}
-
-// exports.findByIdPreConsultaAndIdPersonal = (data, callback) => {
-// 	let q = `
-// 		SELECT * FROM consultas
-// 			WHERE
-// 				id_preconsulta = ? AND
-// 				id_personal = ?
-// 	`
-
-// 	var options = {
-// 		sql: q,
-// 		nestTables: false
-// 	}
-
-// 	return connection.query(options, [data.id_preconsulta, data.id_personal], callback)
-
-// 	connection.end()
-// }
-
 exports.update = (data, callback) => {
 	let q = `
 		UPDATE consultas SET 
-			fecha = ?,
-			fechaPoximaConsulta = ?,
-			id_personal = ?,
+			fechaProximaConsulta = ?,
 			id_diagnostico = ?,
-			observacionDiagnostico = LOWER(?),
-			id_preconsulta = ?
+			observacionDiagnostico = LOWER(?)
 			WHERE
 				id_consulta = ?
 	`
 
-	return connection.query(q, [ data.fecha,
-								 data.fechaPoximaConsulta,
-								 data.id_personal,
+	return connection.query(q, [ data.fechaProximaConsulta,
 								 data.id_diagnostico,
 								 data.observacionDiagnostico.trim(), 
-								 data.id_preconsulta,
 								 data.id_consulta ], callback)
 
 	connection.end()
