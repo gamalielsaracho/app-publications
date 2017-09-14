0fe53cc59e69b09040dc5a6a9cf2f270ab1b678a
gamaliel <fabiogamaliel@gmail.com>
2017-09-14 12:39:02 -0400

    MostrarConsultaPage page created

---
+++ client/src/consulta/pages/MostrarConsultaPage.js
@@ -0,0 +1,11 @@
+import React , { Component } from 'react'
+
+import MostrarConsultaContainer from '../components/Mostrar'
+
+class MostrarConsultaPage extends Component {
+	render() {
+		return <MostrarConsultaContainer/>
+	}
+}
+
+export default MostrarConsultaPage
