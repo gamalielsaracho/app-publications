1f1bcabcc43123209282404d2f8fa145eb061345
gamaliel <fabiogamaliel@gmail.com>
2017-09-14 13:33:46 -0400

    consulta sockets modified

--- server/app/consulta/consulta.sockets.js
+++ server/app/consulta/consulta.sockets.js
@@ -23,27 +23,64 @@ export default (io) => {
 		
 		consultas()
 
-
-		socket.on('crear_consulta', function(data) {
-			Consulta.verifyIfExist(data, (err, consultaExistente) => {
+		// Listar todas las consultas que realizó un médico/a.
+		// para mostrarlo al mismo.
+		socket.on('listar_consultas_medico', (data) => {
+			Consulta.findListByIdPersonal(data.id_personal, (err, consultas) => {
 				if(err) {
 					console.log(err)
+						
+					socket.emit('listar_consultas_medico', { error: 'Lo sentimos, acurrió un error. intente más tarde.' })
+					return
 				}
 
-				if(consultaExistente[0]) {
-						socket.emit('crear_consulta', { error: 'Esta consulta ya está registrado' })
-						return
-				} else {
-					Consulta.create(data, (err) => {
+				consultaNsp.emit('listar_consultas_medico', { consultas: consultas })
+			})
+
+		})
+
+		// Para el historial clinico del paciente.
+		socket.on('listar_consultas_paciente', (data) => {
+			Consulta.findListByIdPaciente(data.id_paciente, (err, consultas) => {
+				if(err) {
+					console.log(err)
+						
+					socket.emit('listar_consultas_paciente', { error: 'Lo sentimos, acurrió un error. intente más tarde.' })
+					return
+				}
+
+				consultaNsp.emit('listar_consultas_paciente', { consultas: consultas })
+			})
+
+		})
+
+		socket.on('crear_consulta', function(data) {
+			// Consulta.verifyIfExist(data, (err, consultaExistente) => {
+				// if(err) {
+				// 	console.log(err)
+				// }
+
+				// if(consultaExistente[0]) {
+				// 		socket.emit('crear_consulta', { error: 'Esta consulta ya está registrado' })
+				// 		return
+				// } else {
+					Consulta.create(data, (err, consulta) => {
 						if(err) {
+							console.log(err)
 							socket.emit('crear_consulta', { error: 'Ocurrió un error, intente más tarde.' })
 							return
 						}
 
+						socket.emit('crear_consulta', { 
+							mensaje: 'Se Agregó exitósamente.',
+							// ...
+							idConsultaInsertada: consulta.insertId
+						})
+
 						consultas()
 					})
-				}
-			})
+				// }
+			// })
 		})
 
 
@@ -61,17 +98,30 @@ export default (io) => {
 			})
 		})
 
+		socket.on('mostrar_consulta_editar', (data) => {
+			Consulta.findByIdToUpdate(data, (err, consulta) => {
+				// console.log(consulta)
 
-		socket.on('editar_consulta', (data) => {
-			Consulta.verifyIfExist(data, (err, consultaExistente) => {
 				if(err) {
 					console.log(err)
-				}
-
-				if(consultaExistente[0]) {
-					socket.emit('editar_consulta', { error: 'Esta consulta ya está registrado' })
+					socket.emit('mostrar_consulta_editar', { error: 'Ocurrió un error, intente más tarde.' })
 					return
 				}
+
+				socket.emit('mostrar_consulta_editar', consulta[0])
+			})
+		})
+
+		socket.on('editar_consulta', (data) => {
+			// Consulta.verifyIfExist(data, (err, consultaExistente) => {
+				// if(err) {
+				// 	console.log(err)
+				// }
+
+				// if(consultaExistente[0]) {
+				// 	socket.emit('editar_consulta', { error: 'Esta consulta ya está registrado' })
+				// 	return
+				// }
 					
 					
 				Consulta.update(data, (err) => {
@@ -83,9 +133,22 @@ export default (io) => {
 
 					socket.emit('editar_consulta', { mensaje: 'Se actualizó exitósamente.' })
 					
+					// Volvemos a busrcar la consulta editada
+					// para emitir los cambios.
+					Consulta.findById(data, (err, consulta) => {
+						if(err) {
+							console.log(err)
+							
+							socket.emit('mostrar_consulta', { error: 'Ocurrió un error, intente más tarde.' })
+							return
+						}
+
+						socket.emit('mostrar_consulta', consulta[0])
+					})
+
 					consultas()
 				})
-			})
+			// })
 		})
 		
 		socket.on('mostrar_consulta', (data) => {
