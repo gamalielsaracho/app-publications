fdfae585013e6f6c9a2b1a1b3019ad8f8d19fb52
gamaliel <fabiogamaliel@gmail.com>
2017-09-14 12:38:43 -0400

    ListarConsultasPage page created

---
+++ client/src/consulta/pages/ListarConsultasPage.js
@@ -0,0 +1,11 @@
+import React , { Component } from 'react'
+
+import ListarConsultasContainer from '../components/Listar'
+
+class ListarConsultasPage extends Component {
+	render() {
+		return <ListarConsultasContainer/>
+	}
+}
+
+export default ListarConsultasPage
