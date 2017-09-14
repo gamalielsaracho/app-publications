97f037377537974e23a737b3d9ecd10794198ff5
gamaliel <fabiogamaliel@gmail.com>
2017-09-14 12:36:21 -0400

    functions changed

--- client/src/consulta/components/Formulario/Formulario.js
+++ client/src/consulta/components/Formulario/Formulario.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import { Field, reset } from 'redux-form'
 import ReactModal from 'react-modal'
 
+import jwtDecode from 'jwt-decode'
+import removeAccents from 'remove-accents'
+
 import _ from 'lodash'
 
 import Cargando from '../../../app/components/Cargando'
 
 import MensajeOerror from '../../../app/components/MensajeOerror'
+
+// DIAGNOSTICO.
+import FieldSelectDiagnosticosContainer from '../../../diagnostico/components/FieldSelectDiagnosticos'
+import FormularioDiagnosticoContainer from '../../../diagnostico/components/Formulario'
+
 
 const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
   <div>
@@ -23,16 +31,45 @@ class Formulario extends Component {
 		super(props)
 		this.enviarFormulario = this.enviarFormulario.bind(this)
 		this.renderCargando = this.renderCargando.bind(this)
+		this.renderFieldTextArea = this.renderFieldTextArea.bind(this)
+
+		this.personalLocalSt = jwtDecode(localStorage.getItem('token'))
+	}
+
+	componentWillMount() {
+		this.props.listarDiagnosticos()
 	}
 
 	enviarFormulario(formProps) {				
-		console.log(formProps)
+		formProps.fecha = new Date()
 
+		formProps.id_preconsulta = parseInt(this.props.idPreConsulta)
+		// El id del personal Logeado (Médico/a).
+		formProps.id_personal = this.personalLocalSt.id_personal
+
+		formProps.id_paciente = this.props.datosCita.cita.id_paciente
+
+		// Para pasarle a push y redireccionar a la consulta creada.
+		formProps.id_cita = this.props.datosCita.cita.id_cita
+
+		// console.log(formProps)
+		
 		if(this.props.editarContenido) {
-			this.props.editarNivel(formProps)
+			this.props.editarConsulta(formProps)
 		} else {
-			this.props.crearNivel(formProps)
+			this.props.crearConsulta(formProps)
 		}
+	}
+
+	renderFieldTextArea({ input, label, type, meta: { touched, error, warning } }) {
+		return <div>
+			<div className="form-group">
+			 	<label htmlFor={label}>{label}</label>
+		    	<textarea className="form-control" {...input} placeholder={label} type={type}>
+		    	</textarea>
+			</div>
+		    { touched && ((error && <label className="text-danger">{ error }</label>)) }
+		</div>
 	}
 
 	renderCargando(cargando) {
@@ -46,7 +83,7 @@ class Formulario extends Component {
 	render() {
 		const customStyles = {
 		    content : {
-		  		height: '40vh',
+		  		height: '50vh',
 		  		position: 'none'
 		  	}
 		}
@@ -54,7 +91,7 @@ class Formulario extends Component {
 		const { handleSubmit, pristine, reset, submitting } = this.props		
 		
 		const { 
-			abirtoCrear, abirtoEditar, cargando, nivel 
+			abirtoCrear, abirtoEditar, cargando, consulta 
 		} = this.props.formulario
 
 		let error = this.props.formulario.error ? this.props.formulario.error 
@@ -69,21 +106,31 @@ class Formulario extends Component {
 					       	style={customStyles}>
 
 				<div className='container'>
-					<h4 className='text-center'>Formulario nivel</h4>
+					<h4 className='text-center'>Formulario consulta</h4>
 
 					<MensajeOerror error={error} mensaje={null}/>
 					<div className='row'>
 						{ this.renderCargando(cargando) }
 
-						<div className='col-xs-12 col-sm-6 col-md-6 col-lg-4'>
+						<FormularioDiagnosticoContainer/>
 
+						<div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
 							<form onSubmit={handleSubmit(this.enviarFormulario)}>
+								<div className='row'>
+									<div className='col-xs-12 col-sm-6 col-md-6 col-lg-4'>
+										<Field name='id_diagnostico' type='text'
+											component={FieldSelectDiagnosticosContainer}
+											listaDiagnosticos={this.props.listaDiagnosticos}
+											label='Diagnóstico'/>
+										<Field name='observacionDiagnostico' type='text' component={this.renderFieldTextArea} label='Observaciones'/>
+									</div>
+									<div className='col-xs-12 col-sm-6 col-md-6 col-lg-4'>
+										<Field name='fechaProximaConsulta' type='date' component={renderField} label='Fecha próxima consulta'/>
+									</div>
+								</div>
 								
-								<Field name='descripcion' type='text' component={renderField} label='Descripción'/>
-														
 								<button type="submit" className="btn btn-info btn-space" disabled={pristine || submitting}>Guardar</button>
-								<button type="button" onClick={ this.props.cerrarFormularioNivel } className="btn btn-primary btn-space">Cancelar</button>
-								
+								<button type="button" onClick={ this.props.cerrarFormularioConsulta } className="btn btn-primary btn-space">Cancelar</button>
 							</form>
 						</div>
 					</div>
