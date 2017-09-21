8494f43e2077e804b4fe0503f8bf907782393021
gamaliel <fabiogamaliel@gmail.com>
2017-09-21 05:11:26 -0400

    diagnosticoConsulta route created

--- server/app/routes.js
+++ server/app/routes.js
@@ -2,9 +2,11 @@
 // import rolRoutes from './rol/rol.routes.js'
 
 import preConsultaParametroRoutes from './preConsultaParametro/preConsultaParametro.routes'
+import diagnosticoConsultaRoutes from './diagnosticoConsulta/diagnosticoConsulta.routes'
 
 export default (app) => {
 	// usuarioRoutes(app)
 	// rolRoutes(app)
 	preConsultaParametroRoutes(app)
+	diagnosticoConsultaRoutes(app)
 }
