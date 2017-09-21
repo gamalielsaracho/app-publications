6921328a49d4426f304917c0dbeeacd5f0b4ba47
gamaliel <fabiogamaliel@gmail.com>
2017-09-21 05:11:04 -0400

    inserts

--- server/sql-scripts/inserts.sql
+++ server/sql-scripts/inserts.sql
@@ -18,3 +18,28 @@ INSERT INTO `ciudades` (`id_ciudad`, `descripcion`, `id_departamento`)
 -- AREAS.
 INSERT INTO `areas` (`id_area`, `descripcion`)
 	VALUES (NULL, 'urbana'), (NULL, 'rural');
+
+
+-- ESPECIALIDADES
+INSERT INTO `especialidades` (`id_especialidad`, `descripcion`) 
+	VALUES (NULL, 'dermatología'), (NULL, 'pediatría'), (NULL, 'urología'), 
+	(NULL, 'reumatología'), (NULL, 'otorrinolaringologia'),
+	(NULL, 'ginecología'), (NULL, 'medicina Interna');
+
+
+-- UNIDADES DE MEDIDAS PARAMENTRO PRE-CONSULTA.
+INSERT INTO `unidadesparametropre` (`id_unidadParametroPre`, `descripcion`) 
+	VALUES (NULL, '%');
+
+-- PARAMETROS PRE-CONSULTA.
+INSERT INTO `parametrospreconsulta` (`id_parametroPreconsulta`, `descripcion`, `valorNormal`, `valorAlto`, `valorBajo`, `id_unidadParametroPre`) 
+	VALUES (NULL, 'temperatura', '43', '65', '12', '1'),
+		   (NULL, 'presión arterial', '321', '80', '42', '1');
+
+-- DIAGNÓSTICOS.
+INSERT INTO `diagnosticos` (`id_diagnostico`, `descripcion`)
+	VALUES (NULL, 'gripe'), (NULL, 'dengue'), (NULL, 'papera');
+
+-- ALERGIAS
+INSERT INTO `alergias` (`id_alergia`, `descripcion`)
+	VALUES (NULL, 'al polen'), (NULL, 'al chocolate'), (NULL, 'al pescado');
