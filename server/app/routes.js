ac64f0654c85d53057b03d56e5dd0926133ce2b7
gamaliel <fabiogamaliel@gmail.com>
2017-09-22 04:01:31 -0400

    diagnosticoConsultaRoutes route deleted

--- server/app/routes.js
+++ server/app/routes.js
@@ -2,11 +2,9 @@
 // import rolRoutes from './rol/rol.routes.js'
 
 import preConsultaParametroRoutes from './preConsultaParametro/preConsultaParametro.routes'
-import diagnosticoConsultaRoutes from './diagnosticoConsulta/diagnosticoConsulta.routes'
 
 export default (app) => {
 	// usuarioRoutes(app)
 	// rolRoutes(app)
 	preConsultaParametroRoutes(app)
-	diagnosticoConsultaRoutes(app)
 }
