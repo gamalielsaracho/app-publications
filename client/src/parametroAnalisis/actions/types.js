5e329f49a9e4e3d989b40b679423828431b35326
gamaliel <fabiogamaliel@gmail.com>
2017-09-22 03:58:08 -0400

    parametroAnalisis actions created

---
+++ client/src/parametroAnalisis/actions/types.js
@@ -0,0 +1,36 @@
+// FIND.
+export const LISTAR_PARAMETROS_ANALISIS_REQUEST = 'listar_parametros_analisis_request'
+export const LISTAR_PARAMETROS_ANALISIS_EXITO = 'listar_parametros_analisis_exito'
+export const LISTAR_PARAMETROS_ANALISIS_FALLO = 'listar_parametros_analisis_fallo'
+
+// EDIT.
+export const ABRIR_FORMULARIO_EDITAR_PARAMETRO_ANALISIS_REQUEST = 'abrir_formulario_editar_parametro_analisis_request'
+export const ABRIR_FORMULARIO_EDITAR_PARAMETRO_ANALISIS_EXITO = 'abrir_formulario_editar_parametro_analisis_exito'
+export const ABRIR_FORMULARIO_EDITAR_PARAMETRO_ANALISIS_FALLO = 'abrir_formulario_editar_parametro_analisis_fallo'
+
+	export const EDITAR_PARAMETRO_ANALISIS_REQUEST = 'edit_parametro_analisis_request'
+	export const EDITAR_PARAMETRO_ANALISIS_EXITO = 'edit_parametro_analisis_exito'
+	export const EDITAR_PARAMETRO_ANALISIS_FALLO = 'edit_parametro_analisis_fallo'
+
+// CREATE.
+export const ABRIR_FORMULARIO_CREAR_PARAMETRO_ANALISIS = 'abrir_formulario_crear_parametro_analisis'
+
+	export const CREAR_PARAMETRO_ANALISIS_REQUEST = 'crear_parametro_analisis_request'
+	export const CREAR_PARAMETRO_ANALISIS_EXITO = 'crear_parametro_analisis_exito'
+	export const CREAR_PARAMETRO_ANALISIS_FALLO = 'crear_parametro_analisis_fallo'
+
+export const CERRAR_FORMULARIO_PARAMETRO_ANALISIS = 'cerrar_formulario_parametro_analisis'
+
+// SHOW.
+export const MOSTRAR_PARAMETRO_ANALISIS_REQUEST = 'mostrar_parametro_analisis_request'
+export const MOSTRAR_PARAMETRO_ANALISIS_EXITO = 'mostrar_parametro_analisis_exito'
+export const MOSTRAR_PARAMETRO_ANALISIS_FALLO = 'mostrar_parametro_analisis_fallo'
+
+export const CERRAR_MODAL_MOSTRAR_PARAMETRO_ANALISIS = 'cerrar_modal_mostrar_parametro_analisis'
+
+// DELETE.
+export const ELIMINAR_PARAMETRO_ANALISIS_REQUEST = 'eliminair_parametro_analisis_request'
+export const ELIMINAR_PARAMETRO_ANALISIS_EXITO = 'eliminair_parametro_analisis_exito'
+export const ELIMINAR_PARAMETRO_ANALISIS_FALLO = 'eliminair_parametro_analisis_fallo'
+
+
