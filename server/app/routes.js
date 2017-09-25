91d9b248e29376c51a5d665034373a9497daa53b
gamaliel <fabiogamaliel@gmail.com>
2017-09-24 23:50:28 -0400

    tipoAnalisisParametroRoutes function called

--- server/app/routes.js
+++ server/app/routes.js
@@ -2,9 +2,11 @@
 // import rolRoutes from './rol/rol.routes.js'
 
 import preConsultaParametroRoutes from './preConsultaParametro/preConsultaParametro.routes'
+import tipoAnalisisParametroRoutes from './tipoAnalisisParametro/tipoAnalisisParametro.routes'
 
 export default (app) => {
 	// usuarioRoutes(app)
 	// rolRoutes(app)
 	preConsultaParametroRoutes(app)
+	tipoAnalisisParametroRoutes(app)
 }
