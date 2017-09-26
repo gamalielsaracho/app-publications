46b040af9265dae2e19cef37e2c8a2a7196272bb
gamaliel <fabiogamaliel@gmail.com>
2017-09-26 03:24:55 -0400

    referencia model created

---
+++ server/app/referencia/referencia.model.js
@@ -0,0 +1,159 @@
+import connection from '../../config/connection'
+
+exports.find = (callback) => {
+	let q = `
+		SELECT
+			*
+		FROM
+			referencias
+	`
+	
+	var options = {
+		sql: q, 
+		nestTables: false
+	}
+
+	return connection.query(options, callback)
+
+	connection.end()
+}
+
+exports.findByIdParametroAnalisis = (idParametroAnalisis, callback) => {
+
+	let q = `
+		SELECT
+			*
+		FROM
+			referencias
+		WHERE
+			id_parametroAnalisis = ?
+	`
+	
+	var options = {
+		sql: q, 
+		nestTables: false
+	}
+
+	return connection.query(options, [idParametroAnalisis], callback)
+
+	connection.end()
+}
+
+exports.findById = (idReferencia, callback) => {
+
+	let q = `
+		SELECT
+			*
+		FROM  
+			referencias 
+		WHERE
+			id_referencia = ?
+	`
+	var options = {
+		sql: q,
+		nestTables: false
+	}
+
+	return connection.query(options, [idReferencia], callback)
+
+	connection.end()
+}
+
+exports.verifyIfExist = (data, callback) => {
+	let q = `
+		SELECT * FROM referencias 
+			WHERE
+				id_parametroAnalisis = ?
+	`
+	return connection.query(q, [ data.id_parametroAnalisis ], callback)
+
+	connection.end()
+}
+
+exports.create = (data, callback) => {
+	let q = `
+		INSERT INTO referencias (
+			id_referencia, diasMaximos, mesesMaximos, anosMaximos, superior,
+			inferior, diasMinimos, mesesMinimos, anosMinimos, sexo, 
+			id_parametroAnalisis
+		)
+			VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+	`
+	
+	return connection.query(q, [ data.diasMaximos,
+								 data.mesesMaximos,
+								 data.anosMaximos,
+								 data.superior,
+								 data.inferior,
+								 data.diasMinimos,
+								 data.mesesMinimos,
+								 data.anosMinimos,
+								 data.sexo,
+								 data.id_parametroAnalisis ], callback)
+
+	connection.end()
+}
+
+exports.findByIdToUpdate = (idReferencia, callback) => {
+	let q = `
+		SELECT
+			*
+		FROM  
+			referencias
+		WHERE
+			id_referencia = ?
+	`
+
+	var options = {
+		sql: q,
+		nestTables: false
+	}
+
+	return connection.query(options, [idReferencia], callback)
+
+	connection.end()
+}
+
+exports.update = (data, callback) => {
+	// console.log(data)
+	let q = `
+		UPDATE referencias SET
+			diasMaximos = ?,
+			mesesMaximos = ?,
+			anosMaximos = ?,
+			superior = ?,
+			inferior = ?,
+			diasMinimos = ?,
+			mesesMinimos = ?,
+			anosMinimos = ?,
+			sexo = ?
+		WHERE
+			id_referencia = ?
+	`
+
+	return connection.query(q, [ data.diasMaximos,
+								 data.mesesMaximos,
+								 data.anosMaximos,
+								 data.superior,
+								 data.inferior,
+								 data.diasMinimos,
+								 data.mesesMinimos,
+								 data.anosMinimos,
+								 data.sexo,
+								 data.id_referencia ], callback)
+
+	connection.end()
+}
+
+exports.delete = (idReferencia, callback) => {	
+
+	let q = `
+		DELETE FROM referencias 
+			WHERE
+				id_referencia = ?
+	`
+
+	return connection.query(q, [idReferencia], callback)
+
+	connection.end()
+}
