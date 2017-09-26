f8128b7c3a75626cb806310faba39cd4dc557236
gamaliel <fabiogamaliel@gmail.com>
2017-09-26 03:26:09 -0400

    referenciaRoutes function called

--- server/app/routes.js
+++ server/app/routes.js
@@ -3,10 +3,12 @@
 
 import preConsultaParametroRoutes from './preConsultaParametro/preConsultaParametro.routes'
 import tipoAnalisisParametroRoutes from './tipoAnalisisParametro/tipoAnalisisParametro.routes'
+import referenciaRoutes from './referencia/referencia.routes'
 
 export default (app) => {
 	// usuarioRoutes(app)
 	// rolRoutes(app)
 	preConsultaParametroRoutes(app)
 	tipoAnalisisParametroRoutes(app)
+	referenciaRoutes(app)
 }
