b2021f24b05dbee86092b15f09b5cece38572148
gamaliel <fabiogamaliel@gmail.com>
2017-09-28 05:26:37 -0400

    analisisSolicitado sockets created

---
+++ server/app/analisisSolicitado/analisisSolicitado.sockets.js
@@ -0,0 +1,152 @@
+import AnalisisSolicitado from './analisisSolicitado.model'
+
+export default (io) => {
+	var analisisSolicitadoNsp = io.of('/analisisSolicitado');
+	
+	analisisSolicitadoNsp.on('connection', function (socket) {
+
+		console.log('Analisis Solicitado Conectado.')
+
+		function analisisSolicitados() {
+			AnalisisSolicitado.find((err, analisisSolicitados) => {
+				// console.log(analisisSolicitados)
+				if(err) {
+					console.log(err)
+					
+					socket.emit('listar_analisisSolicitados', { error: 'Lo sentimos, acurrió un error. intente más tarde.' })
+					return
+				}
+
+				analisisSolicitadoNsp.emit('listar_analisisSolicitados', {
+					analisisSolicitados: analisisSolicitados
+				})
+			})
+		}
+		
+		analisisSolicitados()
+
+		socket.on('listar_analisisSolicitados_porIdPaciente', (data) => {
+			AnalisisSolicitado.findByIdPaciente(data.id_paciente, (err, analisisSolicitados) => {
+				if(err) {
+					console.log(err)
+					socket.emit('listar_analisisSolicitados_porIdPaciente', { error: 'Ocurrió un error, intente más tarde.' })
+					return
+				}
+
+				analisisSolicitadoNsp.emit('listar_analisisSolicitados_porIdPaciente', {
+					analisisSolicitados: analisisSolicitados
+				})
+			})
+		})
+
+
+		socket.on('crear_analisisSolicitado', function(data) {
+			// AnalisisSolicitado.verifyIfExist(data, (err, solicitudExistente) => {
+			// 	if(err) {
+			// 		console.log(err)
+			// 	}
+
+			// 	if(solicitudExistente[0]) {
+			// 			socket.emit('crear_analisisSolicitado', { error: 'Este nivel ya está registrado' })
+			// 			return
+			// 	} else {
+					AnalisisSolicitado.create(data, (err, analisisSolicitado) => {
+						if(err) {
+							socket.emit('crear_analisisSolicitado', { error: 'Ocurrió un error, intente más tarde.' })
+							return
+						}
+
+						socket.emit('crear_analisisSolicitado', { mensaje: 'Se agregó exitósamente.' })
+						
+						analisisSolicitados()
+					})
+			// 	}
+			// })
+		})
+
+
+		socket.on('eliminar_analisisSolicitado', (data) => {
+			AnalisisSolicitado.delete(data, (err) => {
+				if(err) {
+					console.log(err)
+					socket.emit('eliminar_analisisSolicitado', { error: 'Ocurrió un error, intente más tarde.' })
+					return
+				}
+
+				socket.emit('eliminar_analisisSolicitado', { mensaje: 'Se Eliminó exitósamente.' })
+
+				analisisSolicitados()
+			})
+		})
+
+		socket.on('mostrar_analisisSolicitado_editar', (data) => {
+			AnalisisSolicitado.findByIdToEdit(data, (err, analisisSolicitado) => {
+				if(err) {
+					console.log(err)
+					socket.emit('mostrar_analisisSolicitado_editar', { error: 'Ocurrió un error, intente más tarde.' })
+					return
+				}
+
+				socket.emit('mostrar_analisisSolicitado_editar', analisisSolicitado[0])
+			})
+		})
+
+
+		socket.on('editar_analisisSolicitado', (data) => {
+			// AnalisisSolicitado.verifyIfExist(data, (err, solicitudExistente) => {
+			// 	if(err) {
+			// 		console.log(err)
+			// 	}
+
+			// 	if(solicitudExistente[0]) {
+			// 		socket.emit('editar_analisisSolicitado', { error: 'Este nivel ya está registrado' })
+			// 		return
+			// 	}
+					
+					
+				AnalisisSolicitado.update(data, (err) => {
+					if(err) {
+						console.log(err)
+						socket.emit('editar_analisisSolicitado', { error: 'Ocurrió un error, intente más tarde.' })
+						return
+					}
+
+					socket.emit('editar_analisisSolicitado', { mensaje: 'Se actualizó exitósamente.' })
+
+					AnalisisSolicitado.findById(data, (err, analisisSolicitado) => {
+						if(err) {
+							console.log(err)
+							socket.emit('mostrar_analisisSolicitado', { error: 'Ocurrió un error, intente más tarde.' })
+							return
+						}
+
+						socket.emit('mostrar_analisisSolicitado', analisisSolicitado[0])
+					})
+
+					analisisSolicitados()
+				})
+			// })
+		})
+		
+		
+		socket.on('mostrar_analisisSolicitado', (data) => {
+			AnalisisSolicitado.findById(data, (err, analisisSolicitado) => {
+				// console.log(analisisSolicitado)
+
+				if(err) {
+					console.log(err)
+					socket.emit('mostrar_analisisSolicitado', { error: 'Ocurrió un error, intente más tarde.' })
+					return
+				}
+
+				socket.emit('mostrar_analisisSolicitado', analisisSolicitado[0])
+			})
+		})
+
+
+
+		socket.on('disconnect', function () {
+			console.log('Analisis Solicitado Desconecto.')
+		})
+	})
+}
