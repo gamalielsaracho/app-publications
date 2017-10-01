e5a963a0d043dccb72d08937aeecd657574d3c1f
gamaliel <fabiogamaliel@gmail.com>
2017-09-30 20:46:09 -0400

    new structure

--- client/src/analisisSolicitado/components/Mostrar/Mostrar.js
+++ client/src/analisisSolicitado/components/Mostrar/Mostrar.js
@@ -1,65 +1,107 @@
 import React, { Component } from 'react'
+import moment from 'moment'
+
+import jwtDecode from 'jwt-decode'
+import removeAccents from 'remove-accents'
+
 import ReactModal from 'react-modal'
 
 import MensajeOerror from '../../../app/components/MensajeOerror'
 import Cargando from '../../../app/components/Cargando'
 
-// Formulario Modal para EDITAR un parametro.
-import FormularioParametroAnalisisContainer from '../Formulario'
+// Formulario Modal para EDITAR un analisis solicitado.
+import FormularioAnalisisSolicitadoContainer from '../Formulario'
 
 class Mostrar extends Component {
 	constructor(props) {
 		super(props)
-		this.renderParametroAnalisis = this.renderParametroAnalisis.bind(this)
-		this.renderParametroAnalisisFormulario = this.renderParametroAnalisisFormulario.bind(this)
+		this.renderAnalisisSolicitado = this.renderAnalisisSolicitado.bind(this)
+		this.renderAnalisisSolicitadoFormulario = this.renderAnalisisSolicitadoFormulario.bind(this)
+		this.renderEstado = this.renderEstado.bind(this)
+		this.renderOptionsByRol = this.renderOptionsByRol.bind(this)
+
+		this.personalLocalSt = jwtDecode(localStorage.getItem('token'))
 	}
 
 	componentWillMount() {
-		this.props.mostrarParametroAnalisis(this.props.idParametroAnalisis)
+		// this.props.urls es pasado como property al ser llamado.
+		// que contiene todos los parametros existentes en la url 
+		// en el cual está parado el usuario.
+		this.props.mostrarAnalisisSolicitado(this.props.urls.idAnalisisSolicitado)
 	}
 
-	renderParametroAnalisisFormulario() {
+	renderEstado(pendiente) {
+		if(pendiente) {
+			return <span>Pendiente</span>
+		} else {
+			return <span>Realizado</span>
+		}
+	}
+
+	renderAnalisisSolicitadoFormulario() {
 		if(this.props.formulario.abirtoCrear || this.props.formulario.abirtoEditar) {
-			return <FormularioParametroAnalisisContainer/>
+			return <FormularioAnalisisSolicitadoContainer/>
 		} else {
 			return <span></span>
 		}
 	}
 
-	renderParametroAnalisis(dato, cargando) {
+	renderOptionsByRol(personal) {
+		let rol = removeAccents(this.personalLocalSt.rol)
+		let idPersonal = this.personalLocalSt.id_personal
+		let urlToRedirect = ``
+
+		if(this.props.urls.idPaciente) {
+			urlToRedirect = `/dashboard/pacientes/${this.props.urls.idPaciente}/solicitudes-laboratorio`
+		} else if (this.props.urls.idConsulta) {
+			urlToRedirect = `/dashboard/citas/${this.props.urls.idCita}/preconsulta/${this.props.urls.idPreConsulta}/consulta/${this.props.urls.idConsulta}`
+		}
+
+		if(rol == 'administracion' || (rol == 'medico' && personal.id_personal == idPersonal)) {
+			return <div className='container'>
+				<button onClick={ () => { this.props.abrirFormularioEditarAnalisisSolicitado(this.props.urls.idAnalisisSolicitado) } } className='btn btn-warning btn-space'>Editar</button>
+				<button onClick={ () => { this.props.eliminarAnalisisSolicitado(this.props.urls.idAnalisisSolicitado, urlToRedirect) } } className='btn btn-danger btn-space'>Eliminar</button>
+			</div>
+		}
+	}
+
+	renderAnalisisSolicitado(dato, cargando) {
 		if(cargando) {
 			return <Cargando/>
-		} else if(dato) {
-			// console.log(dato)
-			return <div>
-				<div className='row'>
-					{ this.renderParametroAnalisisFormulario() }
+		} else {
+			if(dato) {
+				return <div className='row'>
 
-					<div className='col-xs-12 col-sm-12 col-md-4 col-lg-4'>
-						<h4><strong>Nombre:</strong>{ ' '+ dato.parametro.descripcion }</h4>
+					{ this.renderAnalisisSolicitadoFormulario() }
+
+					<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6'>				            
+						<p><strong>Fecha a realizar:</strong>{ ' '+moment(dato.analisisSolicitado.fechaArealizar).format('LL') }</p>
+						<p><strong>Estado:</strong>{ this.renderEstado(dato.analisisSolicitado.pendiente) }</p>
+
+						<p><strong>Médico/a:</strong>{ ' '+dato.personal.nombres+' '+dato.personal.apellidos }</p>
+						<p><strong>Especialidad:</strong>{ ' '+dato.especialidad.descripcion }</p>
 					</div>
-					<div className='col-xs-12 col-sm-12 col-md-4 col-lg-4'>
-						<h4><strong>Unidad de medida:</strong>{ ' '+ dato.unidad.descripcion }</h4>
+
+					<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
+						<h3>Lista de tipos de análisis solicitados</h3>
 					</div>
-					<div className='col-xs-12 col-sm-12 col-md-4 col-lg-4'>
-						<h4><strong>Tipo de examen:</strong>{ ' '+ dato.tipoExamen.descripcion }</h4>
-					</div>
+					
+					{ this.renderOptionsByRol(dato.personal) }
 				</div>
-				<div className='row'>
-					<button type="button" onClick={() => { this.props.abrirFormularioEditarParametroAnalisis(this.props.idParametroAnalisis) }} className="btn btn-warning btn-space">Editar</button>
-				</div>
-			</div>
+			} else {
+				return <span></span>
+			} 
 		}
 	}
 
 	render() {
 
-		const { cargando, parametroAnalisis, error } = this.props.mostrar
+		const { cargando, analisisSolicitado, error } = this.props.mostrar
 		
 		return <div className=''>
 			<MensajeOerror error={error} mensaje={null}/>
 
-			{ this.renderParametroAnalisis(parametroAnalisis, cargando) }
+			{ this.renderAnalisisSolicitado(analisisSolicitado, cargando) }
 		</div>
 	}
 }
