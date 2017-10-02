54645c867fa79b0471b2d3b7c1779b17b2d098ac
gamaliel <fabiogamaliel@gmail.com>
2017-10-02 10:27:37 -0400

    analisis model created

---
+++ server/app/analisis/analisis.model.js
@@ -0,0 +1,122 @@
+import connection from '../../config/connection'
+
+
+// PUEDE SER MOSTRADA POR ID_ANALISIS O ID_SOLICITUDANALISIS.
+	// (VER HASTA PODER HACERLO).
+// exports.findPreviewById = (data, callback) => {
+	
+// 	let q = `
+// 		SELECT * FROM analisis 
+// 			WHERE
+// 				id_analisis = ?
+// 	`
+// 	var options = {
+// 		sql: q,
+// 		nestTables: false
+// 	}
+
+// 	return connection.query(options, [data.id_analisis], callback)
+
+// 	connection.end()
+// }
+
+exports.find = (callback) => {
+
+	let q = `
+		SELECT * FROM analisis
+	`
+	var options = {
+		sql: q, 
+		nestTables: false
+	}
+
+	return connection.query(options, callback)
+
+	connection.end()
+}
+
+exports.findById = (data, callback) => {
+
+	let q = `
+		SELECT * FROM analisis 
+			WHERE
+				id_analisis = ?
+	`
+	var options = {
+		sql: q,
+		nestTables: false
+	}
+
+	return connection.query(options, [data.id_analisis], callback)
+
+	connection.end()
+}
+
+exports.findByIdanalisisSolicitado = (data, callback) => {
+
+	let q = `
+		SELECT
+			id_analisisSolicitado
+		FROM 
+			analisis 
+		WHERE
+			id_analisisSolicitado = ?
+	`
+	var options = {
+		sql: q,
+		nestTables: false
+	}
+
+	return connection.query(options, [data.id_analisisSolicitado], callback)
+
+	connection.end()
+}
+
+exports.verifyIfExist = (data, callback) => {
+	let q = `
+		SELECT * FROM analisis 
+			WHERE
+			id_analisisSolicitado = ?
+	`
+	return connection.query(q, [data.id_analisisSolicitado], callback)
+
+	connection.end()
+}
+
+exports.create = (data, callback) => {
+	let q = `
+		INSERT INTO analisis (id_analisis, fecha, id_analisisSolicitado, id_personal)
+			VALUES (null, now(), ?, ?);
+	`
+	return connection.query(q, [ data.fecha,
+								 data.id_analisisSolicitado,
+								 data.id_personal ], callback)
+
+	connection.end()
+}
+
+// exports.update = (data, callback) => {
+// 	let q = `
+// 		UPDATE analisis SET 
+// 			descripcion = ?
+// 			WHERE 
+// 				id_analisis = ?
+// 	`
+
+// 	return connection.query(q, [data.descripcion.trim(), data.id_analisis], callback)
+
+// 	connection.end()
+// }
+
+exports.delete = (data, callback) => {	
+
+	let q = `
+		DELETE FROM analisis 
+			WHERE
+				id_analisis = ?
+	`
+
+	return connection.query(q, [data.id_analisis], callback)
+
+	connection.end()
+}
