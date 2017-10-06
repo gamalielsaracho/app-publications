8d41e0b8c1f2046e7c63db628c9a3dfce1463eda
gamaliel <fabiogamaliel@gmail.com>
2017-10-06 04:29:17 -0400

    analisisTipoReferencia model created

---
+++ server/app/analisisTipoReferencia/analisisTipoReferencia.model.js
@@ -0,0 +1,158 @@
+import connection from '../../config/connection'
+
+// ANÁLISI-x-TIPOS X REFERENCIAS.
+exports.find = (idAnalisisTipo, callback) => {
+
+	let q = `
+		SELECT
+			*
+		FROM
+			analisisTiposReferencias analisisTipoReferencia,
+			analisis analisis,
+			referencias referencia,
+			parametrosAnalisis parametro,
+			unidadesAnalisis unidad,
+			tiposExamenes tipoExamen 
+		WHERE
+			analisisTipoReferencia.id_referencia = referencia.id_referencia AND
+			analisisTipoReferencia.id_analisis = analisis.id_analisis AND
+			referencia.id_parametroAnalisis = parametro.id_parametroAnalisis  AND
+			parametro.id_unidadAnalisis = unidad.id_unidadAnalisis AND
+			parametro.id_tipoExamen = tipoExamen.id_tipoExamen AND
+
+			analisisTipoReferencia.id_analisisTipo = ?
+	`
+	
+	var options = {
+		sql: q, 
+		nestTables: true
+	}
+
+	return connection.query(options, [idAnalisisTipo], callback)
+
+	connection.end()
+}
+
+
+exports.verifyIfExist = (data, callback) => {
+	let q = `
+		SELECT * FROM analisisTiposReferencias 
+			WHERE
+				id_analisis = ? AND
+				id_tipoAnalisis = ? AND 
+				id_referencia = ?
+	`
+	return connection.query(q, [ data.id_analisis, 
+								 data.id_tipoAnalisis,
+								 data.id_referencia ], callback)
+
+	connection.end()
+}
+
+exports.create = (data, callback) => {
+	let q = `
+		INSERT INTO analisisTiposReferencias (
+			id_analisisTipoAnalisisReferencia,
+			id_analisisTipo, 
+			id_analisis, 
+			id_tipoAnalisis, 
+			id_referencia,
+			valor
+		)
+		VALUES (null, ?, ?, ?, ?, ?)
+	`
+	if(data.valor) {
+		data.valor = data.valor.toString().trim()
+	}
+
+	return connection.query(q, [ data.id_analisisTipo,
+								 data.id_analisis,
+							     data.id_tipoAnalisis,
+								 data.id_referencia,
+								 data.valor ], callback)
+
+	connection.end()
+}
+
+exports.findById = (idAnalisisTipoAnalisisReferencia, callback) => {
+
+	let q = `
+		SELECT
+			*
+		FROM  
+			analisisTiposReferencias analisisTipoReferencia,
+			referencias referencia,
+			parametrosAnalisis parametro,
+			unidadesAnalisis unidad,
+			tiposExamenes tipoExamen
+		WHERE
+			analisisTipoReferencia.id_referencia = referencia.id_referencia AND
+			referencia.id_parametroAnalisis = parametro.id_parametroAnalisis  AND
+			parametro.id_unidadAnalisis = unidad.id_unidadAnalisis AND
+			parametro.id_tipoExamen = tipoExamen.id_tipoExamen AND
+
+			analisisTipoReferencia.id_analisisTipoAnalisisReferencia = ?
+	`
+
+	var options = {
+		sql: q,
+		nestTables: false
+	}
+
+	return connection.query(options, [idAnalisisTipoAnalisisReferencia], callback)
+
+	connection.end()
+}
+
+exports.findByIdToUpdate = (idAnalisisTipoAnalisisReferencia, callback) => {
+
+	let q = `
+		SELECT
+			*
+		FROM  
+			analisisTiposReferencias
+		WHERE
+			id_analisisTipoAnalisisReferencia = ?
+	`
+
+	var options = {
+		sql: q,
+		nestTables: false
+	}
+
+	return connection.query(options, [idAnalisisTipoAnalisisReferencia], callback)
+
+	connection.end()
+}
+
+exports.update = (data, callback) => {
+	// console.log(data)
+	let q = `
+		UPDATE analisisTiposReferencias SET 
+			valor = ?
+		WHERE
+			id_analisisTipoAnalisisReferencia = ?
+	`
+
+	if(data.valor) {
+		data.valor = data.valor.toString().trim()
+	}
+
+	return connection.query(q, [ data.valor,
+								 data.id_analisisTipoAnalisisReferencia ], callback)
+
+	connection.end()
+}
+
+exports.delete = (idAnalisisTipoAnalisisReferencia, callback) => {	
+
+	let q = `
+		DELETE FROM analisisTiposReferencias 
+			WHERE
+				id_analisisTipoAnalisisReferencia = ?
+	`
+
+	return connection.query(q, [idAnalisisTipoAnalisisReferencia], callback)
+
+	connection.end()
+}
