043ab1aa7f117539d02d4ff0e55ae49d0b827615
gamaliel <fabiogamaliel@gmail.com>
2017-10-07 18:20:56 -0400

    renderInputsByRol function created

--- client/src/analisisSolicitado/components/Formulario/Formulario.js
+++ client/src/analisisSolicitado/components/Formulario/Formulario.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { Field, reset } from 'redux-form'
 import ReactModal from 'react-modal'
 
+import jwtDecode from 'jwt-decode'
+import removeAccents from 'remove-accents'
+
 import _ from 'lodash'
 
 import Cargando from '../../../app/components/Cargando'
@@ -22,6 +25,10 @@ class Formulario extends Component {
 		super(props)
 		this.enviarFormulario = this.enviarFormulario.bind(this)
 		this.renderFormulario = this.renderFormulario.bind(this)
+		this.renderFieldRadio = this.renderFieldRadio.bind(this)
+
+		this.renderInputsByRol = this.renderInputsByRol.bind(this)
+		this.personalLocalSt = jwtDecode(localStorage.getItem('token'))
 	}
 
 	enviarFormulario(formProps) {				
@@ -35,7 +42,43 @@ class Formulario extends Component {
 			formProps.id_consulta = this.props.urls.idConsulta
 			this.props.crearAnalisisSolicitado(formProps)
 		}
-		console.log(formProps)
+
+		// console.log(formProps)
+	}
+
+	renderFieldRadio({ input, name, label, type, value, meta: { touched, error, warning } }) {
+		return <div>
+			<div className='form-group'>
+				<label className="radio-inline">
+			    	<input type={type} name={name} value={value} {...input}/>
+					<strong>{label}</strong>
+				</label>
+				{ touched && ((error && <label className="text-danger">{ error }</label>)) }
+			</div>
+		</div>
+	}
+
+	renderInputsByRol() {
+		let rol = removeAccents(this.personalLocalSt.rol)
+
+		if(rol == 'administracion') {
+			return <div>
+				<Field name='fechaArealizar' type='date' component={renderField} label='Fecha a realizar'/>
+				<Field name='pendiente' type='radio' component={this.renderFieldRadio} value='1' label='Pendiente'/>
+				<Field name='pendiente' type='radio' component={this.renderFieldRadio} value='0' label='Realizado'/>
+			</div>
+		} else {
+			if(rol == 'laboratorio') {
+				return <div>
+					<Field name='pendiente' type='radio' component={this.renderFieldRadio} value='1' label='Pendiente'/>
+					<Field name='pendiente' type='radio' component={this.renderFieldRadio} value='0' label='Realizado'/>
+				</div>
+			} else {
+				return <div>
+					<Field name='fechaArealizar' type='date' component={renderField} label='Fecha a realizar'/>
+				</div>
+			}
+		}
 	}
 
 	renderFormulario(cargando) {
@@ -47,8 +90,8 @@ class Formulario extends Component {
 			return <div className='row'>
 				<div className='col-xs-12 col-sm-6 col-md-6 col-lg-4'>
 					<form onSubmit={handleSubmit(this.enviarFormulario)}>
-						<Field name='fechaArealizar' type='date' component={renderField} label='Fecha a realizar'/>
-														
+						{ this.renderInputsByRol() }
+
 						<button type="submit" className="btn btn-info btn-space" disabled={pristine || submitting}>Guardar</button>
 						<button type="button" onClick={ this.props.cerrarFormularioAnalisisSolicitado } className="btn btn-primary btn-space">Cancelar</button>
 					</form>
