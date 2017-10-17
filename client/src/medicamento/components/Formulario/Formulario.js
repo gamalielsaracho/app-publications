d8f15860c061b39740783f9721549d2d59a9934d
gamaliel <fabiogamaliel@gmail.com>
2017-10-17 06:33:50 -0400

    renderFormulario method created

--- client/src/medicamento/components/Formulario/Formulario.js
+++ client/src/medicamento/components/Formulario/Formulario.js
@@ -13,6 +13,14 @@ import
 	FieldSelectFarmaceuticasContainer
 from '../../../farmaceutica/components/FieldSelectFarmaceuticas'
 
+import
+	FieldSelectNombresMedicamentosContainer
+from '../../../nombreMedicamento/components/FieldSelectNombresMedicamentos'
+
+import
+	FieldSelectPresentacionesContainer
+from '../../../presentacion/components/FieldSelectPresentaciones'
+
 
 const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
   <div>
@@ -28,7 +36,7 @@ class Formulario extends Component {
 	constructor(props) {
 		super(props)
 		this.enviarFormulario = this.enviarFormulario.bind(this)
-		this.renderCargando = this.renderCargando.bind(this)
+		this.renderFormulario = this.renderFormulario.bind(this)
 	}
 
 	// componentWillUnmount() {
@@ -37,6 +45,8 @@ class Formulario extends Component {
 
 	componentWillMount() {
 		this.props.listarFarmaceuticasFuncion()
+		this.props.listarNombresMedicamentosFuncion()
+		this.props.listarPresentacionesFuncion()
 	}
 
 	enviarFormulario(formProps) {				
@@ -49,33 +59,74 @@ class Formulario extends Component {
 		}
 	}
 
-	renderCargando(cargando) {
+
+	renderFormulario(cargando) {
+		const { handleSubmit, pristine, reset, submitting } = this.props		
+
 		if(cargando) {
 			return <Cargando/>
 		} else {
-			return <span></span>
+			return <div className=''>
+
+				<form onSubmit={handleSubmit(this.enviarFormulario)}>																						
+					<div className='row'>
+						<div className='col-xs-12 col-sm-12 col-md-6 col-lg-4'>
+							<Field name='id_farmaceutica'
+								type='text' component={FieldSelectFarmaceuticasContainer}
+								listar={this.props.listarFarmaceuticas}
+								label='Farmacéutica'/>
+						</div>
+						<div className='col-xs-12 col-sm-12 col-md-6 col-lg-4'>
+							<Field name='id_nombreMedicamento'
+								type='text' component={FieldSelectNombresMedicamentosContainer}
+								listar={this.props.listarNombresMedicamentos}
+								label='Nombre comercial'/>
+						</div>
+						<div className='col-xs-12 col-sm-12 col-md-6 col-lg-4'>
+							<Field name='id_presentacion'
+								type='text' component={FieldSelectPresentacionesContainer}
+								listar={this.props.listarPresentaciones}
+								label='Presentación'/>
+						</div>
+					</div>
+
+					<div className='row'>
+						<div className='col-xs-12 col-sm-12 col-md-6 col-lg-4'>
+							<Field name='cantidadXunidad' type='text' component={renderField} label='Cantidad x unidad'/>
+						</div>
+						<div className='col-xs-12 col-sm-12 col-md-6 col-lg-4'>
+							<Field name='observaciones' type='text' component={renderField} label='Observaciones'/>
+						</div>
+					</div>
+
+					<button type="submit" className="btn btn-info btn-space" disabled={pristine || submitting}>Guardar</button>
+					<button type="button" onClick={ this.props.cerrarFormularioMedicamento } className="btn btn-primary btn-space">Cancelar</button>
+				</form>
+			</div>
 		}
 	}
 
 	render() {
 		const customStyles = {
-		    content : {
-			posit: 'absolute',
-			    top: '40px',
-			    left: '0px',
-			    right: '0px',
-			    bottom: '40px',
-			    border: '1px solid #ccc',
-			    background: '#fff',
-			    overflow: 'auto',
-			    WebkitOverflowScrolling: 'touch',
-			    borderRadius: '4px',
-			    outline: 'none',
-			    padding: '20px'
+		 //    content : {
+			// posit: 'absolute',
+			//     top: '40px',
+			//     left: '0px',
+			//     right: '0px',
+			//     bottom: '40px',
+			//     border: '1px solid #ccc',
+			//     background: '#fff',
+			//     overflow: 'auto',
+			//     WebkitOverflowScrolling: 'touch',
+			//     borderRadius: '4px',
+			//     outline: 'none',
+			//     padding: '20px'
+		 //  	}
+		 	content : {
+		  		height: '50vh',
+		  		position: 'none'
 		  	}
 		}
-
-		const { handleSubmit, pristine, reset, submitting } = this.props		
 		
 		const { 
 			abirtoCrear, abirtoEditar, cargando, medicamento 
@@ -97,30 +148,8 @@ class Formulario extends Component {
 					<h4 className='text-center'>Formulario medicamento</h4>
 
 					<MensajeOerror error={error} mensaje={null}/>
-					<div className=''>
-						{ this.renderCargando(cargando) }
-
-
-						<form onSubmit={handleSubmit(this.enviarFormulario)}>																						
-							<div className='row'>
-								<div className='col-xs-12 col-sm-12 col-md-6 col-lg-4'>
-									<Field name='id_farmaceutica'
-										type='text' component={FieldSelectFarmaceuticasContainer}
-										listar={this.props.listarFarmaceuticas}
-										label='Farmacéutica'/>
-								</div>
-								<div className='col-xs-12 col-sm-12 col-md-6 col-lg-4'>
-									<Field name='nombre' type='text' component={renderField} label='Nombre'/>
-								</div>
-								<div className='col-xs-12 col-sm-12 col-md-6 col-lg-4'>
-									<Field name='observaciones' type='text' component={renderField} label='Descripción Gral.'/>
-								</div>
-							</div>
-
-							<button type="submit" className="btn btn-info btn-space" disabled={pristine || submitting}>Guardar</button>
-							<button type="button" onClick={ this.props.cerrarFormularioMedicamento } className="btn btn-primary btn-space">Cancelar</button>
-						</form>
-					</div>
+					
+					{ this.renderFormulario(cargando) }
 				</div>
 			</ReactModal>
 		} else {
