3fdb7c3fa527504a763962b87136816f59977bac
gamaliel <fabiogamaliel@gmail.com>
2017-10-20 15:30:07 -0400

    preconsulta sockets modified

--- server/app/preconsulta/preconsulta.sockets.js
+++ server/app/preconsulta/preconsulta.sockets.js
@@ -1,5 +1,7 @@
 import Preconsulta from './preconsulta.model'
 
+import verifyRef from './././../validations/verifyRef.js'
+
 export default (io) => {
 	var preconsultaNsp = io.of('/preconsulta');
 	
@@ -46,17 +48,41 @@ export default (io) => {
 			})
 
 			socket.on('eliminar_preconsulta', (data) => {
-				Preconsulta.delete(data, (err) => {
+				let d = {
+					table1: 'consultas', 
+					table2: 'citas', 
+					table3: 'preconsultasparametros',
+					fieldPrimaryKey: 'id_preconsulta',
+					primaryKey: data.id_preconsulta
+				}
+
+				verifyRef(d, (err, enUso) => {
 					if(err) {
 						console.log(err)
 						socket.emit('eliminar_preconsulta', { error: 'Ocurrió un error, intente más tarde.' })
 						return
 					}
 
-					socket.emit('eliminar_preconsulta', { mensaje: 'Se Eliminó exitósamente.' })
+					console.log('preConsulta EN USO ---------->')
+					console.log(enUso)
+					
+					if(enUso) {
+						socket.emit('eliminar_preconsulta', { error: 'Este dato está siendo usado por otros registros.' })
+					} else {
+						Preconsulta.delete(data, (err) => {
+							if(err) {
+								console.log(err)
+								socket.emit('eliminar_preconsulta', { error: 'Ocurrió un error, intente más tarde.' })
+								return
+							}
 
-					preconsultas()
+							socket.emit('eliminar_preconsulta', { mensaje: 'Se Eliminó exitósamente.' })
+
+							preconsultas()
+						})
+					}
 				})
+
 			})
 
 
