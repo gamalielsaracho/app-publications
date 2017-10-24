298ea1003bfaabefa612a8ff654a12fc8e0d4bdd
gamaliel <fabiogamaliel@gmail.com>
2017-10-24 18:47:37 -0400

    Mostrar modified

--- client/src/cita/components/Mostrar/Mostrar.js
+++ client/src/cita/components/Mostrar/Mostrar.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react'
 
 import ReactModal from 'react-modal'
 
+import moment from 'moment'
+
 import { formatDate, calcularEdad } from '../../../globalActions'
 
 import MensajeOerror from '../../../app/components/MensajeOerror'
 import Cargando from '../../../app/components/Cargando'
 
+import AnadirPreConsultaContainer from '../../../preconsulta/components/AnadirPreConsulta'
+
+
 class Mostrar extends Component {
 	constructor(props) {
 		super(props)
-		this.renderCargando = this.renderCargando.bind(this)
 		this.renderCita = this.renderCita.bind(this)
 	}
 
@@ -21,24 +25,25 @@ class Mostrar extends Component {
 		this.props.mostrarCita(this.props.idCita)
 	}
 
-	renderCargando(cargando) {
+
+	renderCita(cargando, dato) {
 		if(cargando) {
 			return <Cargando/>
-		} else {
-			return <span></span>
-		}
-	}
+		} else if(dato){
 
-	renderCita(dato) {
-		if(dato.cita != undefined) {
 			return <div className='container'>
-			
+
+					<AnadirPreConsultaContainer datosCita = { dato }/>
+
 					<div className='row'>
 						<div className='col-xs-12 col-sm-12 col-md-4 col-lg-4'>
-							<p><strong>Fecha:</strong>{ dato.cita.fecha }</p>
+							<p><strong>Fecha:</strong>{ moment(dato.cita.fecha).format('L') }</p>
 						</div>
 						<div className='col-xs-12 col-sm-12 col-md-4 col-lg-4'>
-							<p><strong>Hora:</strong>{ dato.cita.hora }</p>
+							<p><strong>Hora inicio:</strong>{ moment(dato.cita.start).format('LT') }</p>
+						</div>
+						<div className='col-xs-12 col-sm-12 col-md-4 col-lg-4'>
+							<p><strong>Hora fin:</strong>{ moment(dato.cita.end).format('LT') }</p>
 						</div>
 					</div>
 
@@ -53,15 +58,13 @@ class Mostrar extends Component {
 						</div>
 						<div className='col-xs-12 col-sm-12 col-md-4 col-lg-4'>
 							<p><strong>Nombre:</strong>{ dato.paciente.nombres +' '+ dato.paciente.apellidos }</p>
-							<p><strong>Edad:</strong> { calcularEdad(dato.paciente.fechaNacimiento) }</p>
+							<p><strong>Fecha de nacimiento:</strong> { moment(dato.paciente.fechaNacimiento).format('L') }</p>
 						</div>
 						<div className='col-xs-12 col-sm-12 col-md-4 col-lg-4'>
 							<p><strong>Sexo:</strong>{ dato.paciente.sexo }</p>
 						</div>
 					</div>
 			</div>
-		} else {
-			return <span></span>
 		}
 	}
 
@@ -76,25 +79,17 @@ class Mostrar extends Component {
 
 		const { cargando, cita, error, abierto } = this.props.mostrar
 
-		console.log("Mostrar estÁ: "+this.props.mostrar.abierto)
+		// console.log("Mostrar estÁ: "+this.props.mostrar.abierto)
 
 		// console.log(cita)
 
-		if(cargando) {
-			return <div className='row'>
-				<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6 col-centered'>
-					{ this.renderCargando(cargando) }
-				</div>
-			</div>
-		} else {
-			return <div className='row'>
-				<MensajeOerror error={error} mensaje={null}/>
+		return <div className='row'>
+			<MensajeOerror error={error} mensaje={null}/>
 
-				{ this.renderCita(cita) }
-				<br/>
-				<br/>
-			</div>
-		}
+			{ this.renderCita(cargando, cita) }
+			<br/>
+			<br/>
+		</div>
 	}
 }
 
