f57543eb25f119c7434b18a996c387c9502a07f4
gamaliel <fabiogamaliel@gmail.com>
2017-10-27 10:54:16 -0400

    Estadistica2  container created

---
+++ client/src/consulta/components/Estadistica2/container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux'
+
+import Estadistica2 from './Estadistica2'
+
+// import {
+// } from '../../actions'
+
+function mapStateToProps(state, ownProps) {
+	// console.log(ownProps)
+	return {
+	}
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Estadistica2)
+
+
