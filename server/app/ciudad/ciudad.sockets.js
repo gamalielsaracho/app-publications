bdd0a90d39656bce4602bb4295981732b8277f5e
gamaliel <fabiogamaliel@gmail.com>
2017-10-29 07:21:31 -0400

    ciudad sockets modified

--- server/app/ciudad/ciudad.sockets.js
+++ server/app/ciudad/ciudad.sockets.js
@@ -2,6 +2,9 @@ import Ciudad from './ciudad.model'
 
 import referentialIntegritySimple from './././../validations/referentialIntegritySimple.js'
 
+import AuditoriaModulo1 from './././../auditoriaModulo1/auditoriaModulo1.model'
+import fieldsToEditData from './././../useFul/fieldsToEditData.js'
+
 export default (socket, io) => {
 	
 		function ciudades() {
@@ -21,21 +24,34 @@ export default (socket, io) => {
 
 
 		socket.on('crear_ciudad', function(data) {
-			Ciudad.create(data, (err, ciudad) => {
+			Ciudad.verifyIfExist(data, (err, ciudadExistente) => {
 				if(err) {
+					console.log(err)
 					socket.emit('crear_ciudad', { error: 'Ocurrió un error, intente más tarde.' })
 					return
 				}
 
-				socket.emit('crear_ciudad', { mensaje: 'Se agregó exitósamente.' })
-			
-				ciudades()
+				if(ciudadExistente[0]) {
+					socket.emit('crear_ciudad', { error: 'Esta ciudad ya está registrada.' })
+				} else {
+					Ciudad.create(data, (err, ciudad) => {
+						if(err) {
+							console.log(err)
+							socket.emit('crear_ciudad', { error: 'Ocurrió un error, intente más tarde.' })
+							return
+						}
+
+						socket.emit('crear_ciudad', { mensaje: 'Se agregó exitósamente.' })
+					
+						ciudades()
+					})
+				}
 			})
 		})
 
 
 		socket.on('mostrar_ciudad', (data) => {
-			Ciudad.findById(data.id_ciudad, (err, ciudad) => {
+			Ciudad.findById(data, (err, ciudad) => {
 				if(err) {
 					socket.emit('mostrar_ciudad', { error: 'Ocurrió un error, intente más tarde.' })
 					return
@@ -57,31 +73,157 @@ export default (socket, io) => {
 				if(enUso[0]) {
 					socket.emit('eliminar_ciudad', { error: 'Este dato está siendo usado por otros registros.' })
 				} else {
-					Ciudad.delete(data.id_ciudad, (err) => {
+					Ciudad.findById(data, (err, ciudadDatosAnterior) => {
+						let cAnt = ciudadDatosAnterior[0]
+
+						// console.log(ciudad)
 						if(err) {
+							console.log(err)
 							socket.emit('eliminar_ciudad', { error: 'Ocurrió un error, intente más tarde.' })
 							return
 						}
 
-						socket.emit('eliminar_ciudad', { mensaje: 'Se Eliminó exitósamente.' })
+						let listaCampos = [
+							{
+								nombreCampo: 'Nombre',
+								datoCampoAnterior: cAnt.ciudad.descripcion
+							},
+							{ 
+								nombreCampo: 'Departamento',
+								datoCampoAnterior: cAnt.departamento.descripcion
+							}
+						]
 
-						ciudades()
-					})
+						Ciudad.delete(data, (err) => {
+							if(err) {
+								socket.emit('eliminar_ciudad', { error: 'Ocurrió un error, intente más tarde.' })
+								return
+							}
+
+							ciudades()
+
+							// console.log(listaCampos)
+							fieldsToEditData(listaCampos, 'eliminación', 'ciudades', data.idPersonal, (err, datos) => {
+								if(err) {
+									console.log(err)
+									socket.emit('eliminar_ciudad', { error: 'Ocurrió un error en la auditoría de este módulo.' })
+									return
+								}
+
+								// .. Ejecutar esto despues de eliminar el registro. 
+								// console.log(datos)
+								AuditoriaModulo1.create(datos, (err) => {
+									if(err) {
+										console.log(err)
+										socket.emit('eliminar_ciudad', { error: 'Ocurrió un error en la auditoría de este módulo.' })
+										return
+									}
+								})
+							})
+
+							socket.emit('eliminar_ciudad', { mensaje: 'Se Eliminó exitósamente.' })
+						})
+
+					})					
 				}
+			})
+		})
+
+		
+		socket.on('mostrar_ciudad_editar', (data) => {
+			Ciudad.findByIdToUpdate(data, (err, ciudad) => {
+				if(err) {
+					socket.emit('mostrar_ciudad_editar', { error: 'Ocurrió un error, intente más tarde.' })
+					return
+				}
+
+				socket.emit('mostrar_ciudad_editar', ciudad[0])
 			})
 		})
 
 
 		socket.on('editar_ciudad', (data) => {
-			Ciudad.update(data, (err) => {
+			// console.log(data)
+			Ciudad.verifyIfExist(data, (err, ciudadExistente) => {
 				if(err) {
+					console.log(err)
 					socket.emit('editar_ciudad', { error: 'Ocurrió un error, intente más tarde.' })
 					return
 				}
 
-				socket.emit('editar_ciudad', { mensaje: 'Se actualizó exitósamente.' })
-			
-				ciudades()
+				if(ciudadExistente[0]) {
+					socket.emit('editar_ciudad', { error: 'Esta ciudad ya está registrada.' })
+				} else {
+					Ciudad.findById(data, (err, ciudadDatosAnterior) => {
+						let cAnt = ciudadDatosAnterior[0]
+
+						// console.log(ciudad)
+						if(err) {
+							console.log(err)
+							socket.emit('editar_ciudad', { error: 'Ocurrió un error, intente más tarde.' })
+							return
+						}
+
+						Ciudad.update(data, (err) => {
+							if(err) {
+								console.log(err)
+								socket.emit('editar_ciudad', { error: 'Ocurrió un error, intente más tarde.' })
+								return
+							}
+
+						
+							ciudades()
+
+							Ciudad.findById(data, (err, ciudadDatosNuevo) => {
+								let cNue = ciudadDatosNuevo[0]
+
+								// console.log(ciudad)
+								if(err) {
+									console.log(err)
+									socket.emit('editar_ciudad', { error: 'Ocurrió un error en la auditoría de este módulo.' })
+									return
+								}
+
+								let listaCampos = [
+									{
+										nombreCampo: 'Nombre',
+										datoCampoAnterior: cAnt.ciudad.descripcion,
+										datoCampoNuevo: cNue.ciudad.descripcion
+									},
+									{ 
+										nombreCampo: 'Departamento',
+										datoCampoAnterior: cAnt.departamento.descripcion,
+										datoCampoNuevo: cNue.departamento.descripcion
+									}
+								]
+
+
+								// console.log(listaCampos)
+								fieldsToEditData(listaCampos, 'actualización', 'ciudades', data.idPersonal, (err, datos) => {
+									if(err) {
+										console.log(err)
+										socket.emit('editar_ciudad', { error: 'Ocurrió un error en la auditoría de este módulo.' })
+										return
+									}
+
+									// .. Ejecutar esto despues de editar el registro. 
+									// console.log(datos)
+
+									AuditoriaModulo1.create(datos, (err) => {
+										if(err) {
+											console.log(err)
+											socket.emit('editar_ciudad', { error: 'Ocurrió un error en la auditoría de este módulo.' })
+											return
+										}
+									})
+								})
+
+							})
+
+							socket.emit('editar_ciudad', { mensaje: 'Se actualizó exitósamente.' })
+						})
+					})
+				}
 			})
 		})
 }
