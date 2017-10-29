f3c0a197a062d67c5c7580e7337bcdd6d07650b7
gamaliel <fabiogamaliel@gmail.com>
2017-10-29 07:20:59 -0400

    ciudad model modified

--- server/app/ciudad/ciudad.model.js
+++ server/app/ciudad/ciudad.model.js
@@ -1,31 +1,106 @@
 import connection from '../../config/connection'
 
 exports.find = (callback) => {
-	return connection.query('SELECT * FROM ciudades', callback)
+	let q = `
+		SELECT 
+			*
+		FROM 
+			ciudades ciudad,
+			departamentos departamento
+		WHERE
+			ciudad.id_departamento = departamento.id_departamento
+	`
+
+	var options = {
+		sql: q, 
+		nestTables: true
+	}
+
+	return connection.query(options, callback)
 
 	connection.end()
 }
 
-exports.findById = (idCiudad, callback) => {
-	return connection.query('select * from ciudades where id_ciudad = ?', [idCiudad], callback)
+exports.findById = (data, callback) => {
+	let q = `
+		SELECT 
+			*
+		FROM 
+			ciudades ciudad,
+			departamentos departamento
+		WHERE
+			ciudad.id_departamento = departamento.id_departamento AND
+			ciudad.id_ciudad = ?
+	`
+
+	var options = {
+		sql: q, 
+		nestTables: true
+	}
+
+	return connection.query(options, [data.id_ciudad], callback)
+
+	connection.end()
+}
+
+exports.verifyIfExist = (data, callback) => {
+	let q = `
+		SELECT
+			* 
+		FROM
+			ciudades 
+		WHERE 
+			descripcion = ? AND
+			id_departamento = ?
+	`
+
+	return connection.query(q, [ data.descripcion.trim(), data.id_departamento ], callback)
 
 	connection.end()
 }
 
 exports.create = (data, callback) => {
-	return connection.query('INSERT INTO ciudades SET ?', data, callback)
+	let q = `
+		INSERT INTO ciudades (id_ciudad, descripcion, id_departamento)
+			VALUES (null, LOWER(?), ?)
+	`
+	return connection.query(q, [ data.descripcion.trim(), data.id_departamento ], callback)
 
 	connection.end()
 }
+
+
+exports.findByIdToUpdate = (data, callback) => {
+	let q = `
+		SELECT 
+			*
+		FROM 
+			ciudades
+		WHERE
+			id_ciudad = ?
+	`
+
+	return connection.query(q, [data.id_ciudad], callback)
+
+	connection.end()
+}
+
 
 exports.update = (data, callback) => {
-	return connection.query('update ciudades set descripcion = ? where id_ciudad = ?', [data.descripcion, data.id_ciudad], callback)
+	let q = `
+		update ciudades set 
+			descripcion = LOWER(?),
+			id_departamento = ?
+		where id_ciudad = ?
+	`
+
+	return connection.query(q, [data.descripcion.trim(), data.id_departamento, data.id_ciudad], callback)
 
 	connection.end()
 }
 
-exports.delete = (idCiudad, callback) => {	
-	return connection.query('DELETE FROM ciudades WHERE id_ciudad = ?', [idCiudad], callback)
+exports.delete = (data, callback) => {	
+	return connection.query('DELETE FROM ciudades WHERE id_ciudad = ?', [data.id_ciudad], callback)
 
 	connection.end()
 }
