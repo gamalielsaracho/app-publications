81df3a3084183b0b29103bc5bee1149c49023256
gamaliel <fabiogamaliel@gmail.com>
2017-10-30 21:09:53 -0400

    renderFormulario method created

--- client/src/area/components/Formulario/Formulario.js
+++ client/src/area/components/Formulario/Formulario.js
@@ -20,7 +20,7 @@ class Formulario extends Component {
 	constructor(props) {
 		super(props)
 		this.enviarFormulario = this.enviarFormulario.bind(this)
-		this.renderCargando = this.renderCargando.bind(this)
+		this.renderFormulario = this.renderFormulario.bind(this)
 	}
 
 	enviarFormulario(formProps) {
@@ -31,12 +31,26 @@ class Formulario extends Component {
 		}
 	}
 
-	renderCargando(cargando) {
+
+	renderFormulario(cargando) {
+		const { handleSubmit, pristine, reset, submitting } = this.props		
+		
 		if(cargando) {
 			return <Cargando/>
 		} else {
-			return <span></span>
+			return <div className='row'>
+				<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
+
+					<form onSubmit={handleSubmit(this.enviarFormulario)}>
+						<Field name='descripcion' type='text' component={renderField} label='Descripción'/>
+														
+						<button type="submit" className="btn btn-info btn-space" disabled={pristine || submitting}>Guardar</button>
+						<button type="button" onClick={ this.props.cerrarFormularioArea } className="btn btn-primary btn-space">Cancelar</button>
+					</form>
+				</div>
+			</div>
 		}
+
 	}
 
 	render() {
@@ -47,11 +61,14 @@ class Formulario extends Component {
 		  	}
 		}
 
-		const { handleSubmit, pristine, reset, submitting } = this.props		
 		
 		const { 
-			abirtoCrear, abirtoEditar, error, cargando, area 
+			abirtoCrear, abirtoEditar, cargando, area 
 		} = this.props.formulario
+
+		let error = this.props.formulario.error ? this.props.formulario.error 
+			: this.props.crear.error ? this.props.crear.error : this.props.editar.error 
+
 
 		let abierto = abirtoEditar ? abirtoEditar : abirtoCrear
 
@@ -63,21 +80,10 @@ class Formulario extends Component {
 				<div className='container'>
 					<h4 className='text-center'>Formulario area</h4>
 
-					<div className='row'>
-						<div className='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
-							<MensajeOerror error={error} mensaje={null}/>
-							{ this.renderCargando(cargando) }
+					<MensajeOerror error={error} mensaje={null}/>
 
-							<form onSubmit={handleSubmit(this.enviarFormulario)}>
-								
-								<Field name='descripcion' type='text' component={renderField} label='Descripción'/>
-														
-								<button type="submit" className="btn btn-info btn-space" disabled={pristine || submitting}>Guardar</button>
-								<button type="button" onClick={ this.props.cerrarFormularioArea } className="btn btn-primary btn-space">Cancelar</button>
-								
-							</form>
-						</div>
-					</div>
+					{ this.renderFormulario(cargando) }
+					
 				</div>
 			</ReactModal>
 		} else {
