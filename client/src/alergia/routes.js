d046fa08c5e2f46ad8789a1a3baf06e940b1aa98
gamaliel <fabiogamaliel@gmail.com>
2017-11-05 21:53:39 -0400

    alergiaRoutes modified

--- client/src/alergia/routes.js
+++ client/src/alergia/routes.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
-import ListarAlergiasAppContainer from './components/ListarApp'
-
 // ALERGIAS.
 import ListarAlergiasPage from './pages/ListarAlergiasPage'
 
-import ListarAuditoria1MovimientosContainer from '../auditoriaModulo1/components/Listar'
-
 const alergiaRoutes = (
-	<Route path='/dashboard/alergias' component={ListarAlergiasAppContainer}>
-		<IndexRoute component={ListarAlergiasPage}/>
-
-		<Route path='/dashboard/alergias/auditoria/:tableName' component={ListarAuditoria1MovimientosContainer}/>
-
-	</Route>
+	<Route path='/dashboard/alergias' component={ListarAlergiasPage}/>
 )
 
 export default alergiaRoutes
