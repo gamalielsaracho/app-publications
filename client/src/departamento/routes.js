6d2ef1d875818dc69cf09a2fb2827f98286fbb74
gamaliel <fabiogamaliel@gmail.com>
2017-11-05 21:59:13 -0400

    departamentoRoutes modified

--- client/src/departamento/routes.js
+++ client/src/departamento/routes.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
-import ListarDepartamentosAppContainer from './components/ListarApp'
 import ListarDepartamentosPage from './pages/ListarDepartamentosPage'
 
-import ListarAuditoria1MovimientosContainer from '../auditoriaModulo1/components/Listar'
-
 const departamentoRoutes = (
-	<Route path='/dashboard/departamentos' component={ListarDepartamentosAppContainer}>
-		<IndexRoute component={ListarDepartamentosPage}/>
-		<Route path='/dashboard/departamentos/auditoria/:tableName' component={ListarAuditoria1MovimientosContainer}/>
-
-	</Route>
+	<Route path='/dashboard/departamentos' component={ListarDepartamentosPage}/>
 )
 
 export default departamentoRoutes
