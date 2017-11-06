e56fcca790f8b69e71375a8a4276120533c06142
gamaliel <fabiogamaliel@gmail.com>
2017-11-05 21:53:52 -0400

    areaRoutes modified

--- client/src/area/routes.js
+++ client/src/area/routes.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
-import ListarAreasAppContainer from './components/ListarApp'
-
 // AREAS.
 import ListarAreasPage from './pages/ListarAreasPage'
 
-import ListarAuditoria1MovimientosContainer from '../auditoriaModulo1/components/Listar'
-
 const areaRoutes = (
-	<Route path='/dashboard/areas' component={ListarAreasAppContainer}>
-		<IndexRoute component={ListarAreasPage}/>
-
-		<Route path='/dashboard/areas/auditoria/:tableName' component={ListarAuditoria1MovimientosContainer}/>
-
-	</Route>
+	<Route path='/dashboard/areas' component={ListarAreasPage}/>		
 )
 
 export default areaRoutes
