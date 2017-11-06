4b0b5a1934fd74bfc8868192a013c5ff4adfaf3a
gamaliel <fabiogamaliel@gmail.com>
2017-11-05 21:59:47 -0400

    farmaceuticaRoutes modified

--- client/src/farmaceutica/routes.js
+++ client/src/farmaceutica/routes.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
-import ListarFarmaceuticasAppContainer from './components/ListarApp'
-
 import ListarFarmaceuticasPage from './pages/ListarFarmaceuticasPage'
 
-import ListarAuditoria1MovimientosContainer from '../auditoriaModulo1/components/Listar'
-
 const farmaceuticaRoutes = (
-	<Route path='/dashboard/farmaceuticas' component={ListarFarmaceuticasAppContainer}>
-		<IndexRoute component={ListarFarmaceuticasPage}/>
-
-		<Route path='/dashboard/farmaceuticas/auditoria/:tableName' component={ListarAuditoria1MovimientosContainer}/>
-
-	</Route>
+	<Route path='/dashboard/farmaceuticas' component={ListarFarmaceuticasPage}/>
 )
 
 export default farmaceuticaRoutes
+
