83a2847cc807b17dfefad9086b4673230dd7c368
gamaliel <fabiogamaliel@gmail.com>
2017-11-06 22:31:53 -0400

    unidadParametroPreRoutes modified

--- client/src/unidadParametroPre/routes.js
+++ client/src/unidadParametroPre/routes.js
@@ -1,21 +1,11 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
-import ListarUnidadesParametroPreAppContainer from './components/ListarApp'
-
 // UNIDADES DE MEDIDA PRE-CONSULTA.
 import ListarUnidadesParametroPrePage from './pages/ListarUnidadesParametroPrePage'
 
-import ListarAuditoria1MovimientosContainer from '../auditoriaModulo1/components/Listar'
-
-
 const unidadParametroPreRoutes = (
-	<Route path='/dashboard/unidades-parametro-preconsulta' component={ListarUnidadesParametroPreAppContainer}>
-		<IndexRoute component={ListarUnidadesParametroPrePage}/>
-
-		<Route path='/dashboard/unidades-parametro-preconsulta/auditoria/:tableName' component={ListarAuditoria1MovimientosContainer}/>
-
-	</Route>
+	<Route path='/dashboard/unidades-parametro-preconsulta' component={ListarUnidadesParametroPrePage}/>
 )
 
 export default unidadParametroPreRoutes
