0005e438ec1ed3f5dc5ecd8719fd03cb0e017d1b
gamaliel <fabiogamaliel@gmail.com>
2017-11-06 22:32:40 -0400

    datos variable changed

--- server/app/consultaSintoma/consultaSintoma.controller.js
+++ server/app/consultaSintoma/consultaSintoma.controller.js
@@ -1,5 +1,7 @@
 import ConsultaSintoma from './consultaSintoma.model'
 
+import AuditoriaModulo1 from './././../auditoriaModulo1/auditoriaModulo1.model'
+import fieldsToEditData from './././../useFul/fieldsToEditData.js'
 
 exports.listar = function(req, res, next) {
 	let idConsulta = req.params.idConsulta
@@ -82,48 +84,134 @@ exports.crear = function(req, res, next) {
 }
 
 exports.editar = function(req, res, next) {
-	let datos = {
+	let data = {
 		id_consultaSintoma: req.body.id_consultaSintoma,
 		id_sintoma: req.body.id_sintoma,
 		id_consulta: req.body.id_consulta,
-		observaciones: req.body.observaciones
+		observaciones: req.body.observaciones,
+		idPersonal: req.body.idPersonal
 	}
 
-	ConsultaSintoma.update(datos, (err) => {
+	ConsultaSintoma.findById(data.id_consultaSintoma, (err, sintomaConsultaDatosAnterior) => {
+		let siCoAnt = sintomaConsultaDatosAnterior[0]
+
 		if(err) {
 			console.log(err)
 			return res.status(422).json({ error: 'Ocurrió un error, intente más tarde.' })
 		}
 
-		ConsultaSintoma.findById(datos.id_consultaSintoma, (err, sintomaConsulta) => {
-			// console.log(sintomaConsulta)
-
+		ConsultaSintoma.update(data, (err) => {
 			if(err) {
 				console.log(err)
 				return res.status(422).json({ error: 'Ocurrió un error, intente más tarde.' })
 			}
 
-			return res.json({ 
-				mensaje: 'Se actualizó exitósamente.',
-				sintomaConsultaActualizado: sintomaConsulta[0]
+			ConsultaSintoma.findById(data.id_consultaSintoma, (err, sintomaConsultaDatosNuevo) => {
+				let siCoNue = sintomaConsultaDatosNuevo[0]
+
+				if(err) {
+					console.log(err)
+					return res.status(422).json({ error: 'Ocurrió un error en la auditoría de este módulo.' })
+				}
+
+				let listaCampos = [
+					{ 
+						nombreCampo: 'Nombre',
+						datoCampoAnterior: siCoAnt.sintoma.descripcion,
+						datoCampoNuevo: siCoNue.sintoma.descripcion
+					},
+					{ 
+						nombreCampo: 'Observaciones',
+						datoCampoAnterior: siCoAnt.consultaSintoma.observaciones,
+						datoCampoNuevo: siCoNue.consultaSintoma.observaciones
+					}
+				]
+
+
+				// console.log(listaCampos)
+				fieldsToEditData(data.id_consultaSintoma, listaCampos, 'actualización', 'consulta-sintomas', data.idPersonal, data.id_consulta, (err, datos) => {
+					if(err) {
+						console.log(err)
+						return res.json({ error: 'Ocurrió un error en la auditoría de este módulo.' })
+					}
+
+					// .. Ejecutar esto despues de editar el registro. 
+					console.log(datos)
+
+					AuditoriaModulo1.create(datos, (err) => {
+						if(err) {
+							console.log(err)
+							return res.json({ error: 'Ocurrió un error en la auditoría de este módulo.' })
+						}
+					})
+				})
+
+				return res.json({ 
+					mensaje: 'Se actualizó exitósamente.',
+					sintomaConsultaActualizado: siCoNue
+				})
 			})
 		})
 
 	})
+
 }
 
 exports.eliminar = function(req, res, next) {
 	let idConsultaSintoma = req.params.idConsultaSintoma
+	let idPersonal = req.params.idPersonal
 
-	ConsultaSintoma.delete(idConsultaSintoma, (err, result) => {
+	ConsultaSintoma.findById(idConsultaSintoma, (err, sintomaConsultaDatosAnterior) => {
+		let siCoAnt = sintomaConsultaDatosAnterior[0]
+
 		if(err) {
 			console.log(err)
-			return res.json({ error: 'Ocurrió un error, intente más tarde.' })
+			return res.status(422).json({ error: 'Ocurrió un error, intente más tarde.' })
 		}
 
-		return res.json({ 
-			mensaje: 'Se Eliminó exitósamente.',
-			id_consultaSintoma: idConsultaSintoma
+		let listaCampos = [
+			{ 
+				nombreCampo: 'Nombre',
+				datoCampoAnterior: siCoAnt.sintoma.descripcion
+			},
+			{ 
+				nombreCampo: 'Observaciones',
+				datoCampoAnterior: siCoAnt.consultaSintoma.observaciones
+			}
+		]
+
+
+		ConsultaSintoma.delete(idConsultaSintoma, (err, result) => {
+			if(err) {
+				console.log(err)
+				return res.json({ error: 'Ocurrió un error, intente más tarde.' })
+			}
+
+
+			// console.log(listaCampos)
+			fieldsToEditData(idConsultaSintoma, listaCampos, 'eliminación', 'consulta-sintomas', idPersonal, siCoAnt.consultaSintoma.id_consulta, (err, datos) => {
+				if(err) {
+					console.log(err)
+					return res.json({ error: 'Ocurrió un error en la auditoría de este módulo.' })
+				}
+
+				// .. Ejecutar esto despues de eliminar el registro. 
+				console.log(datos)
+
+				AuditoriaModulo1.create(datos, (err) => {
+					if(err) {
+						console.log(err)
+						return res.json({ error: 'Ocurrió un error en la auditoría de este módulo.' })
+					}
+				})
+			})
+
+			return res.json({ 
+				mensaje: 'Se Eliminó exitósamente.',
+				id_consultaSintoma: idConsultaSintoma
+			})
 		})
+		
 	})
+
 }
