ba03d9558e0b6efdf38973e053c752d422cd04d8
gamaliel <fabiogamaliel@gmail.com>
2017-11-09 23:56:07 -0400

    buttons added

--- client/src/auditoriaModulo1/components/MostrarModulos/MostrarModulos.js
+++ client/src/auditoriaModulo1/components/MostrarModulos/MostrarModulos.js
@@ -11,29 +11,29 @@ class MostrarModulos extends Component {
 			<div className='row'>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 				  	<Link to='/dashboard/modulos-auditados/areas'>
-						<button type="button" className="btn btn-primary btn-lg">
-							Areas
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Areas
 						</button>
 				  	</Link>
 				</div>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 					<Link to='/dashboard/modulos-auditados/niveles'>
-						<button type="button" className="btn btn-primary btn-lg">
-							Niveles
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Niveles
 						</button>
 				  	</Link>
 				</div>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 					<Link to='/dashboard/modulos-auditados/pacientes'>
-						<button type="button" className="btn btn-primary btn-lg">
-							Pacientes
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Pacientes
 						</button>
 				  	</Link>
 				</div>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 					<Link to='/dashboard/modulos-auditados/departamentos'>
-						<button type="button" className="btn btn-primary btn-lg">
-							Departamentos
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Departamentos
 						</button>
 				  	</Link>
 				</div>
@@ -44,29 +44,29 @@ class MostrarModulos extends Component {
 			<div className='row'>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 				  	<Link to='/dashboard/modulos-auditados/ciudades'>
-						<button type="button" className="btn btn-primary btn-lg">
-							Ciudades
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Ciudades
 						</button>
 				  	</Link>
 				</div>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 					<Link to='/dashboard/modulos-auditados/alergias'>
-						<button type="button" className="btn btn-primary btn-lg">
-							alérgias
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> alérgias
 						</button>
 				  	</Link>
 				</div>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 					<Link to='/dashboard/modulos-auditados/unidades-parametro-preconsulta'>
-						<button type="button" className="btn btn-primary btn-lg">
-							Unidades pre-consulta
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Unidades pre-consulta
 						</button>
 				  	</Link>
 				</div>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 					<Link to='/dashboard/modulos-auditados/parametros-preconsulta'>
-						<button type="button" className="btn btn-primary btn-lg">
-							Parametros pre-consulta
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Parametros pre-consulta
 						</button>
 				  	</Link>
 				</div>
@@ -77,29 +77,29 @@ class MostrarModulos extends Component {
 			<div className='row'>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 					<Link to='/dashboard/modulos-auditados/diagnosticos'>
-						<button type="button" className="btn btn-primary btn-lg">
-							diagnósticos
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> diagnósticos
 						</button>
 				  	</Link>
 				</div>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 				  	<Link to='/dashboard/modulos-auditados/sintomas'>
-						<button type="button" className="btn btn-primary btn-lg">
-							Síntomas
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Síntomas
 						</button>
 				  	</Link>
 				</div>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 					<Link to='/dashboard/modulos-auditados/'>
-						<button type="button" className="btn btn-primary btn-lg">
-							-----
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> -----
 						</button>
 				  	</Link>
 				</div>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 					<Link to='/dashboard/modulos-auditados/'>
-						<button type="button" className="btn btn-primary btn-lg">
-							-----
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> -----
 						</button>
 				  	</Link>
 				</div>
@@ -110,23 +110,31 @@ class MostrarModulos extends Component {
 			<div className='row'>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
 				  	<Link to='/dashboard/modulos-auditados/farmaceuticas'>
-						<button type="button" className="btn btn-primary btn-lg">
-							Farmaceuticas
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Farmaceuticas
 						</button>
 				  	</Link>
 				</div>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
-					<Link to='/dashboard/modulos-auditados'>
-						<button type="button" className="btn btn-primary btn-lg">
-							-----
+					<Link to='/dashboard/modulos-auditados/nombres-medicamentos'>
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Nombres medicamentos
 						</button>
 				  	</Link>
 				</div>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
-
+					<Link to='/dashboard/modulos-auditados/presentaciones'>
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Presentaciones
+						</button>
+				  	</Link>
 				</div>
 				<div className='col-xs-12 col-sm-3 col-md-3 col-lg-3'>
-
+					<Link to='/dashboard/modulos-auditados/drogas'>
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Drogas
+						</button>
+				  	</Link>
 				</div>
 			</div>
 
