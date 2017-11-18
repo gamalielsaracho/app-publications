c5326f8d44fced347431a838c79c5f78c9a60176
gamaliel <fabiogamaliel@gmail.com>
2017-11-18 07:38:59 -0400

    preconsulta model modified

--- server/app/preconsulta/preconsulta.model.js
+++ server/app/preconsulta/preconsulta.model.js
@@ -1,16 +1,28 @@
 import connection from '../../config/connection'
 
+import getHour from '../useFul/getHour'
+
+
 import moment from 'moment'
 
 exports.find = (callback) => {
 
 	let q = `
-		SELECT * FROM preconsultas preconsulta, pacientes paciente,
-		niveles nivel, personales personal
+		SELECT 
+			* 
+		FROM 
+			preconsultas preconsulta, 
+			pacientes paciente,
+			niveles nivel,
+			personales personal,
+			tiposdocumentos tpDocPaciente,
+			tiposdocumentos tpDocEnfermera
 		WHERE
 			preconsulta.id_paciente = paciente.id_paciente AND
+			paciente.id_tipoDocumento = tpDocPaciente.id_tipoDocumento AND
 			preconsulta.id_nivel = nivel.id_nivel AND
-			preconsulta.id_personal = personal.id_personal 
+			preconsulta.id_personal = personal.id_personal AND
+			personal.id_tipoDocumento = tpDocEnfermera.id_tipoDocumento
 	`
 	var options = {
 		sql: q, 
@@ -89,24 +101,47 @@ exports.findById = (data, callback) => {
 exports.create = (data, callback) => {	
 
 	let q = `
-		INSERT INTO preconsultas (id_preconsulta, fecha, id_paciente, id_nivel, id_personal)
-			VALUES (null, ?, ?, ?, ?)
+		INSERT INTO preconsultas (id_preconsulta, fecha, hora, id_nivel, id_paciente, id_personal)
+			VALUES (null, now(), ?, ?, ?, ?)
 	`
-	return connection.query(q, [data.fecha, data.id_paciente, data.id_nivel, data.id_personal], callback)
+	return connection.query(q, [getHour(), data.id_nivel, data.id_paciente, data.id_personal], callback)
 
 	connection.end()
 }
 
+
+exports.findByIdToUpdate = (data, callback) => {
+
+	let q = `
+		SELECT 
+			*
+		FROM 
+			preconsultas 
+		WHERE
+			id_preconsulta = ? 
+	`
+	var options = {
+		sql: q,
+		nestTables: false
+	}
+
+	return connection.query(options, [data.id_preconsulta], callback)
+
+	connection.end()
+}
+
+
 exports.update = (data, callback) => {
 	let q = `
 		UPDATE preconsultas SET 
-			fecha = ?, id_nivel = ?
+			id_paciente = ?, 
+			id_nivel = ?
 			WHERE 
 				id_preconsulta = ?
 	`
 
 	return connection.query(q, [
-		data.fecha, data.id_nivel, data.id_preconsulta
+		data.id_paciente, data.id_nivel, data.id_preconsulta
 	], callback)
 
 	connection.end()
