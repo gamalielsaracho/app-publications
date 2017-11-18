93c985e6506a6975e28de513d6dd22bc6932fe94
gamaliel <fabiogamaliel@gmail.com>
2017-11-18 07:23:24 -0400

    abrirFormularioCrearPaciente actions called

--- client/src/paciente/components/FieldSelectPacientes/container.js
+++ client/src/paciente/components/FieldSelectPacientes/container.js
@@ -1,18 +1,23 @@
 import { connect } from 'react-redux'
 
 import {
+	abrirFormularioCrearPaciente
 } from '../../actions'
 
 import FieldSelectPacientes from './FieldSelectPacientes'
 
 function mapStateToProps(state) {
 	return {
-		
+		// Para mostrar el formulario únicamente si está abierto.
+		formulario: state.paciente.formulario
 	}
 }
 
 function mapDispatchToProps(dispatch) {
 	return {
+		abrirFormularioCrearPaciente: () => {
+			dispatch(abrirFormularioCrearPaciente())
+		}
 	}
 }
 
