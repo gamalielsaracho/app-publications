7cb18cd0d68b5424e20d5ce4dd44115b31469a7c
gamaliel <fabiogamaliel@gmail.com>
2017-11-20 15:30:48 -0400

    new structure

--- client/src/preconsulta/components/MostrarApp/MostrarApp.js
+++ client/src/preconsulta/components/MostrarApp/MostrarApp.js
@@ -9,71 +9,74 @@ import MostrarPreConsultaContainer from '../Mostrar'
 // PreConsulta X Parametro
 import ListarPreConsultaParametrosContainer from '../../../preConsultaParametro/components/Listar'
 
+// Formulario Modal para agregar solo una consulta por pre-consulta.
+import FormularioConsultaContainer from '../../../consulta/components/Formulario'
+
 class MostrarApp extends Component {
 	constructor(props) {
 		super(props)
-		this.renderMenuById = this.renderMenuById.bind(this)
+		this.renderMenu = this.renderMenu.bind(this)
 
 		this.personalLocalSt = jwtDecode(localStorage.getItem('token'))
 	}
 
-	// componentWillMount() {
-	// 	this.props.listarConsultasFuncion()
-	// }
+	componentWillMount() {
+		this.props.mostrarConsultaPorIdPersonalYidPreConsulta(this.personalLocalSt.id_personal, this.props.urls.idPreConsulta)
+	}
 
-	renderMenuById(listar) {
-		let rol = this.personalLocalSt.id_rol
-
-		let urlListarConsultas = `/dashboard/pre-consultas/${this.props.urls.idPreConsulta}/consultas`
-		let urlMostrarConsulta = `/dashboard/pre-consultas/${this.props.urls.idPreConsulta}/consultas/${this.props.urls.idConsulta}`
-
-		let activeListarConsultas = ''
-		let activeMostrarConsulta = ''
-
-		switch(this.props.pathname) {
-			case urlListarConsultas:
-				activeListarConsultas = 'active'
-				break
-
-			case urlMostrarConsulta:
-				activeMostrarConsulta = 'active'
-				break
-		}
-
+	renderMenu(listar) {
 		// 1 médico.
 		// 3 administación.
+		let rol = this.personalLocalSt.id_rol
 
-		if((rol == 1) || (rol == 3)) {
+		let activeMostrarConsulta = ''
+
+		if(this.props.urls.idPreConsulta && this.props.urls.idConsulta) {
+			activeMostrarConsulta = 'active'
+		}
+
+		const { cargando, consulta, error } = this.props.mostrarPorIdPersonalYidPreConsulta
+
+		if(!cargando && consulta) {
+			// console.log('consulta ............---->')
+			// console.log(consulta)
+
 			return <div>
 				<ul className="nav nav-tabs no-print-data">
-				  <li className="nav-item nav-link" className={activeListarConsultas}>
-				  	<Link to={urlListarConsultas}>Listar Consultas</Link>
-				  </li>
-				  <li className="nav-item nav-link" className={activeMostrarConsulta}>
-				  	<a>Detalle Consulta</a>
+					<li className="nav-item nav-link" className={activeMostrarConsulta}>
+						<Link to={`/dashboard/pre-consultas/${this.props.urls.idPreConsulta}/consulta/${consulta.id_consulta}`}>Consulta</Link>
+					</li>
+				</ul>
+			</div>
+		} else {
+			return <div>
+				<FormularioConsultaContainer
+						idPreConsulta={this.props.urls.idPreConsulta}/>
+
+				<ul className="nav nav-tabs no-print-data">
+				  <li className="nav-item nav-link">
+					<button onClick={ this.props.abrirFormularioCrearConsulta } className='btn btn-success'>Agregar</button>
+				  	<br/>
+				  	<br/>
 				  </li> 
 				</ul>
 			</div>
-
-		} else {
-			return <span></span>
 		}
-
 	}
 
 	render() {
-
 		return <div>
 			<br/>
+
 			<MostrarPreConsultaContainer 
 				idPreConsulta={this.props.urls.idPreConsulta}/>
 
 			{/*  
 			*/}
-			
+
 			<ListarPreConsultaParametrosContainer idPreConsulta={this.props.urls.idPreConsulta}/>
 
-			{ this.renderMenuById() }
+			{ this.renderMenu() }
 
 			{ this.props.children }
 
