5c4c1d9bea306fdc50830a91afd5d1a2efe1f975
gamaliel <fabiogamaliel@gmail.com>
2017-11-19 22:27:32 -0400

    new routes created

--- client/src/preconsulta/routes.js
+++ client/src/preconsulta/routes.js
@@ -5,10 +5,32 @@ import FiltrosPreConsultasAppContainer from './components/FiltrosApp'
 
 import ListarPreConsultasAppContainer from './components/ListarApp'
 
+// PRE-CONSULTA APP.
+import MostrarPreConsultaAppContainer from './components/MostrarApp'
+
+// CONSULTAS APP.
+import ListarConsultasAppContainer from '../consulta/components/ListarApp'
+
+import ListarConsultasContainer from '../consulta/components/Listar'
+
+
 const preConsultaRoutes = (
 	<Route path='/dashboard/pre-consultas' component={ListarPreConsultasAppContainer}>
 		<IndexRoute component={FiltrosPreConsultasAppContainer}/>
 		
+		<Route path='/dashboard/consultas' component={ListarConsultasAppContainer}>
+			<IndexRoute component={ListarConsultasContainer}/>
+		</Route>
+
+		<Route path='/dashboard/pre-consultas/:idPreConsulta' component={MostrarPreConsultaAppContainer}>
+			{/*  
+				<IndexRoute component={}/>
+			*/}
+			<Route path='/dashboard/pre-consultas/:idPreConsulta/consultas' component={ListarConsultasContainer}>
+			</Route>			
+
+		</Route>
+
 		{/* <Route path='/dashboard/pre-consultas/:idPreConsulta' component={}/> */}
 
 	</Route>
