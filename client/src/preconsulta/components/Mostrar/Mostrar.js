e0a75c8d4854288507ad51a92cce1fa2fcf4140b
gamaliel <fabiogamaliel@gmail.com>
2017-11-24 09:41:40 -0400

    new methods
    renderDatosPaciente and renderBtnAuditByRol method created

--- client/src/preconsulta/components/Mostrar/Mostrar.js
+++ client/src/preconsulta/components/Mostrar/Mostrar.js
@@ -20,15 +20,73 @@ class Mostrar extends Component {
 
 		this.formularioPreConsultaParametroByRol = this.formularioPreConsultaParametroByRol.bind(this)
 		this.personalLocalSt = jwtDecode(localStorage.getItem('token'))
+		this.renderDatosPaciente = this.renderDatosPaciente.bind(this)
+
+		this.renderBtnAuditByRol = this.renderBtnAuditByRol.bind(this)
 	}
 
 	componentWillMount() {
 		this.props.mostrarPreConsulta(this.props.idPreConsulta)
 	}
 
-	formularioPreConsultaParametroByRol() {
-		let idRol = this.personalLocalSt.id_rol
+	renderBtnAuditByRol() {
+		let rol = this.personalLocalSt.id_rol
 		
+		if(rol == 3) {
+			return <div className='row'>
+				<div className='col-xs-11 col-sm-11 col-md-11 col-lg-11 text-right'>
+					<br/>
+					<Link target="_blank" to={`/dashboard/modulos-auditados/${this.props.urls.idConsulta}/auditoria/consulta-sintomas`}>
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Auditoría
+						</button>
+					</Link>
+				</div>
+			</div>
+		} else {
+			return <span></span>
+		}
+	}
+
+	renderDatosPaciente(preConsulta) {
+		if(this.props.urls.idPaciente) {
+			return <span></span>
+		} else {
+			return <div>
+				<h3>Paciente</h3>
+				<div className='row'>
+					<div className='col-xs-12 col-sm-12 col-md-3 col-lg-3'>
+						<p><strong>Documento:</strong>{ preConsulta.paciente.nroDocumento }</p>
+					</div>
+					<div className='col-xs-12 col-sm-12 col-md-3 col-lg-3'>
+						<p><strong>Nombre:</strong>{ preConsulta.paciente.nombres +' '+ preConsulta.paciente.apellidos }</p>
+					</div>
+					<div className='col-xs-12 col-sm-12 col-md-3 col-lg-3'>
+						<p><strong>Sexo:</strong>{ preConsulta.paciente.sexo }</p>
+					</div>
+					<div className='col-xs-12 col-sm-12 col-md-3 col-lg-3'>
+						<p><strong>Fecha de nacimiento:</strong>{ moment(preConsulta.paciente.fechaNacimiento).format('DD-MM-YYYY') }</p>
+					</div>
+				</div>
+				<div className='row'>
+					<div className='col-xs-12 col-sm-12 col-md-3 col-lg-3'>
+						<p><strong>Telefono:</strong>{ preConsulta.paciente.telefono }</p>
+					</div>
+					<div className='col-xs-12 col-sm-12 col-md-3 col-lg-3'>
+						<p><strong>Celular:</strong>{ preConsulta.paciente.celular }</p>
+					</div>
+					<div className='col-xs-12 col-sm-12 col-md-3 col-lg-3'>
+						<p><strong>Dirección:</strong>{ preConsulta.paciente.direccion }</p>
+					</div>
+				</div>
+				<br/>
+			</div>
+		}
+	}
+
+	formularioPreConsultaParametroByRol(idPreConsulta) {
+		let idRol = this.personalLocalSt.id_rol
+
 		// 2 enfermeria.
 		// 3 administración.
 		if((idRol == 2) || (idRol == 3)) {
@@ -39,24 +97,48 @@ class Mostrar extends Component {
 		}
 	}
 
+	renderBtnAuditByRol() {
+		let rol = this.personalLocalSt.id_rol
+		
+		if(rol == 3) {
+			return <div className='row'>
+				<div className='col-xs-11 col-sm-11 col-md-11 col-lg-11 text-right'>
+					<br/>
+					<Link target="_blank" to={`/dashboard/modulos-auditados/${this.props.idPreConsulta}/auditoria/pre-consulta-parametro`}>
+						<button type="button" className="btn btn-primary btn-md">
+							<span className="glyphicon glyphicon-search" aria-hidden="true"></span> Auditoría
+						</button>
+					</Link>
+				</div>
+			</div>
+		} else {
+			return <span></span>
+		}
+	}
+
 	renderPreConsulta(cargando, preConsulta) {
 		if(cargando) {
 			return <Cargando/>
 		} else if (preConsulta){
-			return <div className='row'>
-				<div className='col-xs-12 col-sm-12 col-md-5 col-lg-5'>
-					<p><strong>Enfermero/a:</strong>{ preConsulta.personal.nombres +' '+ preConsulta.personal.apellidos }</p>
-					<p><strong>Fecha:</strong>{ moment(preConsulta.preconsulta.fecha).format('L') }</p>
-					<p><strong>Nivel:</strong>{ preConsulta.nivel.descripcion }</p>				
-					
-				  	<Link target="_blank" to={`/dashboard/modulos-auditados/${this.props.idPreConsulta}/auditoria/pre-consulta-parametro`}>
-				  		Auditoría
-				  	</Link>
 
+			return <div>
+				{ this.renderDatosPaciente(preConsulta) }
+
+				<div className='row'>
+					<div className='col-xs-12 col-sm-12 col-md-5 col-lg-5'>
+						<p><strong>Enfermero/a:</strong>{ preConsulta.personal.nombres +' '+ preConsulta.personal.apellidos }</p>
+						<p><strong>Fecha:</strong>{ moment(preConsulta.preconsulta.fecha).format('DD-MM-YYYY') } <strong>Hora:</strong>{ preConsulta.preconsulta.hora }</p>
+						<p></p>
+						
+						<p><strong>Observaciones:</strong>{ preConsulta.preconsulta.observaciones }</p>				
+						
+					</div>
+					<div className='col-xs-12 col-sm-12 col-md-6 col-lg-6'>
+						{ this.formularioPreConsultaParametroByRol(preConsulta.preconsulta.id_preconsulta) }
+					</div>
 				</div>
-				<div className='col-xs-12 col-sm-12 col-md-6 col-lg-6'>
-					{ this.formularioPreConsultaParametroByRol() }
-				</div>
+
+				{ this.renderBtnAuditByRol() }
 			</div>
 		}
 	}
