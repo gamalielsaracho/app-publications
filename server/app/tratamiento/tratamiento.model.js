f67979f773a29b5c316d1ee525eba73f17a15e80
gamaliel <fabiogamaliel@gmail.com>
2017-11-26 07:02:42 -0400

    tratamiento model created

---
+++ server/app/tratamiento/tratamiento.model.js
@@ -0,0 +1,45 @@
+import connection from '../../config/connection'
+
+exports.find = (callback) => {
+	return connection.query('SELECT * FROM tratamientos', callback)
+
+	connection.end()
+}
+
+exports.findByIdConsulta = (data, callback) => {
+	let q = `
+		SELECT
+			* 
+		FROM 
+			tratamientos
+		WHERE
+			id_consulta = ?
+	`
+	return connection.query(q, [ data.id_consulta ], callback)
+
+	connection.end()
+}
+
+exports.findById = (data, callback) => {
+	return connection.query('select * from tratamientos where id_tratamiento = ?', [data.id_tratamiento], callback)
+
+	connection.end()
+}
+
+
+exports.create = (data, callback) => {
+	let q = `
+		INSERT INTO tratamientos (id_tratamiento, id_consulta)
+			VALUES (null, ?);
+	`
+	return connection.query(q, [ data.id_consulta ], callback)
+
+	connection.end()
+}
+
+
+exports.delete = (data, callback) => {	
+	return connection.query('DELETE FROM tratamientos WHERE id_tratamiento = ?', [data.id_tratamiento], callback)
+
+	connection.end()
+}
