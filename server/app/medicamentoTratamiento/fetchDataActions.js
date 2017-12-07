c1c84d79a0aaabe010d59e7427f6f55dcd53a0ce
gamaliel <fabiogamaliel@gmail.com>
2017-12-07 11:48:53 -0400

    fetchDataActions created

---
+++ server/app/medicamentoTratamiento/fetchDataActions.js
@@ -0,0 +1,71 @@
+import MedicamentoTratamiento from './medicamentoTratamiento.model'
+import Medicamento from '../medicamento/medicamento.model'
+import MedicamentoDroga from '../medicamentoDroga/medicamentoDroga.model'
+
+
+export default (nsp, socket) => {
+
+	return {
+		listarByIdTratamiento: (data) => {
+			MedicamentoTratamiento.findListByIdTratamiento(data, (err, medicamentosTratamiento) => {
+				// console.log(medicamentosTratamiento)
+				if(err) {
+					console.log(err)
+						
+					socket.emit('listar_medicamentosXtratamientos_byIdTratamiento', { 
+						error: 'Lo sentimos, acurrió un error. intente más tarde.' 
+					})
+					return
+				}
+
+				
+				let longMedicamentosTratamiento = medicamentosTratamiento.length
+
+				if(longMedicamentosTratamiento) {
+					medicamentosTratamiento.map((i) => {
+						let idMedicamento = i.indicacion.id_medicamento
+
+						// console.log('idMedicamento ----> '+idMedicamento)
+
+						Medicamento.findById({ id_medicamento: idMedicamento }, (err, medicamento) => {
+								
+							if(medicamento[0]) {
+								i.detalleMedicamento = medicamento[0]
+							}
+
+
+							MedicamentoDroga.find(idMedicamento , (err, drogas) => {
+								if(err) {
+									console.log(err)
+									socket.emit('listar_medicamentosXtratamientos_byIdTratamiento', { error: 'Lo sentimos, acurrió un error. intente más tarde.' })
+								}
+									
+								if(i.detalleMedicamento) {
+									i.detalleMedicamento.drogas = drogas
+								} else {
+									i.detalleMedicamento = {}
+									i.detalleMedicamento = null
+								}
+
+								// console.log(i)
+
+								if(i == medicamentosTratamiento[longMedicamentosTratamiento - 1]) {
+									nsp.emit('listar_medicamentosXtratamientos_byIdTratamiento', { 
+										medicamentosTratamiento: medicamentosTratamiento 
+									})
+								}
+							})
+						})
+					})				
+
+				} else {
+					nsp.emit('listar_medicamentosXtratamientos_byIdTratamiento', { 
+						medicamentosTratamiento: medicamentosTratamiento 
+					})
+				}
+
+			})
+		}
+		
+	}
+}
